6eb404bf98ca42ca897f5fae6f678a7d5be5db4c
Linver23 <gabrielecavallaro@hotmail.com>
2019-11-11 12:30:50 +0100

    Inizialized folders and paths

--- lib/public/data-stages/paths.lua
+++ lib/public/data-stages/paths.lua
@@ -1,11 +1,41 @@
 -- -- -- PATHS
+
+-- -- GRAPHICS PATHS
 -----------------------------------------------------------------------------------------------------------------
--- -- Icons paths
-kr_icon_path                      = "__Krastorio2-graphics__/graphics/icons/"
-kr_entities_icon_path             = "__Krastorio2-graphics__/graphics/icons/entities/"
-kr_stacked_icon_path              = "__Krastorio2-graphics__/graphics/icons/stacked/"
--- -- Equipments icons paths
-kr_vehicle_equipment_icons_path   = "__Krastorio2-graphics__/graphics/icons/equipment/vehicle/"
-kr_universal_equipment_icons_path = "__Krastorio2-graphics__/graphics/icons/equipment/universal/"
-kr_character_equipment_icons_path = "__Krastorio2-graphics__/graphics/icons/equipment/character/"
+kr_graphic_mod                         = "__Krastorio2__/graphics/"
 -----------------------------------------------------------------------------------------------------------------
+-- Icons paths
+kr_icons_path                          = kr_graphic_mod + "icons/"
+kr_arrows_icons_path                   = kr_icons_path  + "arrows/"
+kr_entities_icons_path                 = kr_icons_path  + "entities/"
+kr_recipes_icons_path                  = kr_icons_path  + "recipes/"
+kr_resources_icons_path                = kr_icons_path  + "resources/"
+kr_retextured_science_packs_icons_path = kr_icons_path  + "retextured-science-packs/"
+kr_technologies_icons_path             = kr_icons_path  + "technologies/"
+kr_signals_icons_path                  = kr_icons_path  + "signals/"
+kr_stacked_icons_path                  = kr_graphic_mod + "compatibility/deadlock-beltboxes-loaders/"
+-- Equipments paths
+kr_equipments_icon_path                = kr_icon_path   + "equipments/"
+kr_vehicle_equipments_icons_path       = kr_equipments_icon_path + "vehicle/"
+kr_universal_equipments_icons_path     = kr_equipments_icon_path + "universal/"
+kr_character_equipments_icons_path     = kr_equipments_icon_path + "character/"
+-----------------------------------------------------------------------------------------------------------------
+-- Equipments 
+kr_equipments_path                     = kr_graphic_mod + "equipments/"
+-----------------------------------------------------------------------------------------------------------------
+-- Entities
+kr_entities_sprites_path               = kr_graphic_mod + "entities/"
+kr_entities_path                       = kr_entities_sprites_path
+-----------------------------------------------------------------------------------------------------------------
+-- Others
+kr_others_path                         = kr_graphic_mod + "others/"
+-----------------------------------------------------------------------------------------------------------------
+-- Tiles
+kr_tiles_path                          = kr_graphic_mod + "tiles/"
+-----------------------------------------------------------------------------------------------------------------
+-- -- SOUNDS PATHS
+-----------------------------------------------------------------------------------------------------------------
+kr_sounds_mod                          = "__Krastorio2__/sounds/"
+-----------------------------------------------------------------------------------------------------------------
+kr_buildings_sounds_path               = kr_sounds_mod + "buildings/"
+kr_weapons_sounds_path                 = kr_sounds_mod + "weapons/"
