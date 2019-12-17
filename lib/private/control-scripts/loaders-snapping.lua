eca0b425e8b704876b605dce9c18f2fcc54a28c8
Linver23 <gabrielecavallaro@hotmail.com>
2019-12-17 11:47:51 +0100

    Improving loader script

--- lib/private/control-scripts/loaders-snapping.lua
+++ lib/private/control-scripts/loaders-snapping.lua
@@ -2,6 +2,7 @@
 -- -- -- CONSTANTS
 
 -- Krastorio loader names
+local KR_LOADERS_GROUP_NAME = "kr-loaders"
 local KRASTORIO_LOADERS =
 {
 	["kr-loader"]          = true,
@@ -11,46 +12,50 @@ local KRASTORIO_LOADERS =
 	["kr-se-loader"]       = true
 }
 
+-- Event filters
 local KRASTORIO_LOADER_BUILT_EVENT_FILTER =
 {
-    {
-		filter = "name", 
-        name   = "kr-loader"
-    },
-    {
-		filter = "name", 
-        name   = "kr-fast-loader"
-    },
-    {
-		filter = "name", 
-        name   = "kr-express-loader"
-    },
-    {
-		filter = "name", 
-        name   = "kr-superior-loader"
-    },
-    {
-		filter = "name", 
-        name   = "kr-se-loader"
+	-- Not a ghost
+	{
+		filter = "ghost", 
+        invert = true
+    },	
+	-- Snappable
+	{
+		filter = "type", 
+        type    = "transport-belt"
     },
 	{
-		filter = "ghost_name",
+		filter = "type", 
+        type    = "underground-belt"
+    },
+	{
+		filter = "type", 
+        type    = "splitter"
+    },
+	{
+		filter = "type", 
+        type    = "loader"
+    },
+	-- Loaders
+    {
+		filter = "name", 
         name   = "kr-loader"
     },
     {
-		filter = "ghost_name",
+		filter = "name", 
         name   = "kr-fast-loader"
     },
     {
-		filter = "ghost_name",
+		filter = "name", 
         name   = "kr-express-loader"
     },
     {
-		filter = "ghost_name",
+		filter = "name", 
         name   = "kr-superior-loader"
     },
     {
-		filter = "ghost_name",
+		filter = "name", 
         name   = "kr-se-loader"
     }
 }
@@ -58,10 +63,10 @@ local KRASTORIO_LOADER_BUILT_EVENT_FILTER =
 -- Type of entities that snap with loaders
 local SNAP_TYPES = 
 {
-	["loader"] = true,
-	["splitter"] = true,
+	["transport-belt"] = true,
 	["underground-belt"] = true,
-	["transport-belt"] = true
+	["splitter"] = true,
+	["loader"] = true
 }
 
 --[[
@@ -134,19 +139,32 @@ local function getEffectiveType(entity)
 end
 
 -- Return the entity, the name, and the type,
+-- of entity in the direction x, y from the given entity,
+-- can return nil values
+-- @entity
+local function getEntityFromEntity(entity, offset_x, offset_y)
+	local position = 
+	{
+		x = entity.position.x + offset_x,
+		y = entity.position.y + offset_y
+	}
+	local entity      = entity.surface.find_entities({position, position})[1] or nil
+	local entity_name = getEffectiveName(entity)
+	local entity_type = getEffectiveType(entity)
+	return entity, entity_name, entity_type
+end
+
+-- Return the entity, the name, and the type,
 -- of entity in front from the given entity,
 -- can return nil values
 -- @entity
 local function getFrontEntity(entity)
-	local front_position = 
-	{
-		x = entity.position.x + FRONT_POSITION_DIFFERENCE[entity.direction][1],
-		y = entity.position.y + FRONT_POSITION_DIFFERENCE[entity.direction][2]
-	}
-	local front_entity      = entity.surface.find_entities({front_position, front_position})[1] or nil
-	local front_entity_name = getEffectiveName(front_entity)
-	local front_entity_type = getEffectiveType(front_entity)
-	return front_entity, front_entity_name, front_entity_type
+	return getEntityFromEntity
+	(
+		entity, 
+		FRONT_POSITION_DIFFERENCE[entity.direction][1], 
+		FRONT_POSITION_DIFFERENCE[entity.direction][2]
+	)
 end
 
 -- Return the entity, the name, and the type,
@@ -154,15 +172,12 @@ end
 -- can return nil values
 -- @entity
 local function getBackEntity(entity)
-	local back_position =
-	{
-		x = entity.position.x + BACK_POSITION_DIFFERENCE[entity.direction][1],
-		y = entity.position.y + BACK_POSITION_DIFFERENCE[entity.direction][2]
-	}	
-	local back_entity      = entity.surface.find_entities({back_position, back_position})[1] or nil
-	local back_entity_name = getEffectiveName(back_entity)
-	local back_entity_type = getEffectiveType(back_entity)
-	return back_entity, back_entity_name, back_entity_type
+	return getEntityFromEntity
+	(
+		entity, 
+		BACK_POSITION_DIFFERENCE[entity.direction][1], 
+		BACK_POSITION_DIFFERENCE[entity.direction][2]
+	)
 end
 
 -- Change entity direction to the oppose side
@@ -173,13 +188,14 @@ end
 
 -- Return if the entity have an inventory
 -- @entity, to check if has an inventory
-local function hasInventory(entity)
-	local entity_type = getEffectiveType(entity)
+local function hasInventory(entity, entity_type)
+	local e_entity_type = entity_type or getEffectiveType(entity) or ""
+	if SNAP_TYPES[e_entity_type] then return false end
 	if 
-		entity_type == "container" or 
-		entity_type == "assembling-machine" or 
-		entity_type == "furnace" or
-		entity_type == "rocket-silo"
+		e_entity_type == "container" or 
+		e_entity_type == "assembling-machine" or 
+		e_entity_type == "furnace" or
+		e_entity_type == "rocket-silo"
 	then
 		return true
 	end
@@ -198,46 +214,58 @@ local function snapLoader(loader)
 	local front_entity, _, front_entity_type = getFrontEntity(loader)
 	-- get entity in behind loader (should be the side when the loader is attached to something)
 	local back_entity, _, back_entity_type   = getBackEntity(loader)
+	-- getting more precise information
+	local front_snap, front_has_inventory = false, false
+	local back_snap,  back_has_inventory  = false, false
+	if front_entity and SNAP_TYPES[front_entity_type] then
+		front_snap = true
+	end
+	if front_entity and not front_snap and hasInventory(front_entity, front_entity_type) then
+		front_has_inventory = true
+	end
+	if back_entity and SNAP_TYPES[back_entity_type] then
+		back_snap = true
+	end
+	if back_entity and not back_snap and hasInventory(back_entity, back_entity_type) then
+		back_has_inventory = true
+	end
 	
 	-- snapping
 	if front_entity and back_entity then -- both
-		if SNAP_TYPES[front_entity_type] and SNAP_TYPES[back_entity_type] or
-		   hasInventory(front_entity) and hasInventory(back_entity)
-		then
+		if (front_snap and back_snap) or (front_has_inventory and back_has_inventory) then
 			return nil
 		end
-		if SNAP_TYPES[back_entity_type] then
+		if back_snap then
 			reverseEntity(loader)
 			front_entity, back_entity = back_entity, front_entity
+			front_snap, back_snap = back_snap, front_snap
+			front_has_inventory, back_has_inventory = back_has_inventory, front_has_inventory			
 		end
-		if hasInventory(front_entity) then
-			reverseEntity(loader)
-			front_entity, back_entity = back_entity, front_entity
+		if back_has_inventory then
+			if OPPOSITE_DIRECTIONS[loader.direction] == front_entity.direction then
+				loader.loader_type = "input"
+			else
+				loader.loader_type = "output"
+			end
+		elseif front_has_inventory then
 			if OPPOSITE_DIRECTIONS[loader.direction] == back_entity.direction then
 				loader.loader_type = "input"
 			else
 				loader.loader_type = "output"
 			end
 		end
-		if hasInventory(back_entity) then
-			if OPPOSITE_DIRECTIONS[loader.direction] == front_entity.direction then
-				loader.loader_type = "input"
-			else
-				loader.loader_type = "output"
-			end
-		end
 	elseif front_entity and not back_entity then -- only front
-		if SNAP_TYPES[front_entity_type] then
+		if front_snap then
 			if OPPOSITE_DIRECTIONS[loader.direction] == front_entity.direction then
 				loader.loader_type = "input"
 			else
 				loader.loader_type = "output"
 			end
-		elseif hasInventory(front_entity) then
+		elseif front_has_inventory then
 			reverseEntity(loader)
 		end
 	elseif not front_entity and back_entity then -- only back
-		if SNAP_TYPES[back_entity_type] then
+		if back_snap then
 			reverseEntity(loader)
 			if OPPOSITE_DIRECTIONS[loader.direction] == back_entity.direction then
 				loader.loader_type = "input"
@@ -258,90 +286,60 @@ local function snapLoaderDropDirection(loader)
 	if front_entity_type and SNAP_TYPES[front_entity_type] then
 		if OPPOSITE_DIRECTIONS[loader.direction] == front_entity.direction then
 			loader.loader_type = "input"
+		else
+			loader.loader_type = "output"			
 		end
 	end	
 end
 -----------------------------------------------------------------------------
 -- -- -- CALLBACKS
-local function onPreMinedEntity(event) 
-	local tags = event.entity.tags
-	if tags then
-		for key, tag in pairs(tags) do
-			game.print("Key: "..tostring(key).."|Value: "..tostring(tag))
-		end
-	end
-end
 
 -- @event, on_built_entity or on_robot_built_entity
 local function onBuiltAnEntity(event)
-	local loader = event.created_entity
-	local name   = getEffectiveName(loader)
-	local type   = getEffectiveType(loader)
-	game.print("test")
-	-- Check requisites for work on loader
-	if 
-		type == "loader" and 
-		loader.valid and
-		KRASTORIO_LOADERS[name] and
-		not event.revived
-	then
-		if event.tags then
-			for key, tag in pairs(event.tags) do
-				game.print("Key2: "..tostring(key).."|Value: "..tostring(tag))
-			end
-		end
-
-		if loader.name == "entity-ghost" then
-			loader.tags = {[name]=true}
-		end
-		snapLoader(loader)
-		return true
-	end
-	return false
-end
-
--- @event, on_robot_built_entity
-local function onRobotBuiltAnEntity(event)
 	local entity = event.created_entity
-	local type   = getEffectiveType(entity) 
-	if not onBuiltAnEntity(event) and SNAP_TYPES[type] then
-		-- get entity in front of the belt		
-		local front_entity, front_entity_name, front_entity_type = getFrontEntity(entity)
-		-- get entity in behind the belt
-		local back_entity, back_entity_name, back_entity_type = getBackEntity(entity)
-		
-		-- if that entities are loaders, snap it! (but only drop type)
-		if
-			front_entity and
-			front_entity_type == "loader" and
-			front_entity.valid and
-			front_entity_name and
-			KRASTORIO_LOADERS[front_entity_name]
-		then
-			snapLoaderDropDirection(front_entity)
-		end
-		if
-			back_entity and
-			back_entity_type == "loader" and
-			back_entity.valid and
-			back_entity_name and
-			KRASTORIO_LOADERS[back_entity_name]
-		then
-			snapLoaderDropDirection(back_entity)
-		end
-	end	
+	if entity.valid and not event.revived and entity.name ~= "entity-ghost" then
+		local name = getEffectiveName(entity)
+		local type = getEffectiveType(entity) 
+		-- Check if is a krastorio loader
+		if KRASTORIO_LOADERS[name] then
+			snapLoader(entity)
+			game.print("snap")
+		elseif SNAP_TYPES[type] then
+			-- get entity in front of the belt		
+			local front_entity, front_entity_name, front_entity_type = getFrontEntity(entity)
+			-- get entity in behind the belt
+			local back_entity, back_entity_name, back_entity_type = getBackEntity(entity)
+			
+			-- if that entities are loaders, snap it! (but only drop type)
+			if
+				front_entity and
+				front_entity.valid and
+				front_entity_name and
+				KRASTORIO_LOADERS[front_entity_name]
+			then
+				snapLoaderDropDirection(front_entity)
+			end
+			if
+				back_entity and
+				back_entity.valid and
+				back_entity_name and
+				KRASTORIO_LOADERS[back_entity_name]
+			then
+				snapLoaderDropDirection(back_entity)
+			end
+		end	
+	end
 end
 -----------------------------------------------------------------------------
 -- -- -- ADDING CALLBACKS
 if settings.startup["loaders"] and settings.startup["loaders"].value then
 	return
-	{
+	{ 
 		-- -- Actions
 		-- When bulding, if its a loader check for snapping and snap, if snapped or not snapping then add to list,
-		-- check anything else built and check for loaders around it they may need correcting.
-		{ onPreMinedEntity, "on_pre_player_mined_item", KRASTORIO_LOADER_BUILT_EVENT_FILTER},
+		-- check anything else built and check for loaders around it they may need correcting.		
 		{ onBuiltAnEntity, "on_built_entity", KRASTORIO_LOADER_BUILT_EVENT_FILTER },
-		{ onRobotBuiltAnEntity, "on_robot_built_entity", KRASTORIO_LOADER_BUILT_EVENT_FILTER }		
+		{ onBuiltAnEntity, "on_robot_built_entity", KRASTORIO_LOADER_BUILT_EVENT_FILTER }		
 	}
 else
 	return {}
