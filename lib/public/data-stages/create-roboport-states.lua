8994f17d0ddf570f9400e192aacdb370400c4a61
Linver23 <gabrielecavallaro@hotmail.com>
2020-02-06 10:15:57 +0100

    Fixes

--- lib/public/data-stages/create-roboport-states.lua
+++ lib/public/data-stages/create-roboport-states.lua
@@ -1,13 +1,5 @@
 local create_roboport_states = {}
 
-local function removeFlag(flags, flat_to_remove)
-	for i, flag in pairs(flags) do
-		if flag == flat_to_remove then
-			table.remove(flags, i)
-		end
-	end
-end
-
 function create_roboport_states.createRoboportVariations(roboport_name)
 	local roboport = krastorio.entities.getEntity("roboport", roboport_name)
 	if roboport then
@@ -16,7 +8,7 @@ function create_roboport_states.createRoboportVariations(roboport_name)
 		logistic_roboport.name = logistic_roboport.name .. "-logistic-mode"
 		logistic_roboport.localised_name = {"entity-name." .. roboport.name}
 		logistic_roboport.localised_description = {"entity-description." .. roboport.name}
-		removeFlag(logistic_roboport.flags, "placeable-player")
+		logistic_roboport.placeable_by = {item = roboport.name, count = 1}
 		logistic_roboport.construction_radius = 0		
 		
 		-- Construction
@@ -24,7 +16,7 @@ function create_roboport_states.createRoboportVariations(roboport_name)
 		construction_roboport.name = construction_roboport.name .. "-construction-mode"
 		construction_roboport.localised_name = {"entity-name." .. roboport.name}
 		construction_roboport.localised_description = {"entity-description." .. roboport.name}
-		removeFlag(construction_roboport.flags, "placeable-player")
+		construction_roboport.placeable_by = {item = roboport.name, count = 1}
 		construction_roboport.logistics_radius = 0
 		
 		data:extend({logistic_roboport, construction_roboport})
