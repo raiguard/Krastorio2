714491093147920a0f0e4f50c63b2af254f3fb25
Linver23 <gabrielecavallaro@hotmail.com>
2020-03-19 22:38:08 +0100

    Redundacy fix on the SchallUraniumProcessing compatibility

--- compatibility-scripts/control/SchallUranium.lua
+++ compatibility-scripts/control/SchallUranium.lua
@@ -1,7 +1,8 @@
-function addRadioactiveItems()
-	if script.active_mods["SchallUraniumProcessing"] then
+local function addRadioactiveItems()
+	if not global.krastorio.schall_uranium_processing_compatibility and script.active_mods["SchallUraniumProcessing"] then
 		table.insert(global.krastorio.radioactive_items, "uranium-concentrate")
 		table.insert(global.krastorio.radioactive_items, "uranium-low-enriched")
+		global.krastorio.schall_uranium_processing_compatibility = true
 	end
 end
 
