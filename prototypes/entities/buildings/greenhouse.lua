c3ad68257b72c6148daee72c6c28db9fab6b7605
Linver23 <gabrielecavallaro@hotmail.com>
2020-03-22 15:15:56 +0100

    Updates

--- prototypes/entities/buildings/greenhouse.lua
+++ prototypes/entities/buildings/greenhouse.lua
@@ -38,7 +38,8 @@ data:extend(
 				production_type = "input",
 				pipe_picture = kr_pipe_path,
 				pipe_covers = pipecoverspictures(),				
-				base_area = 100,
+				base_area = 50,
+				height = 2,
 				base_level = -1,
 				pipe_connections = 
 				{
