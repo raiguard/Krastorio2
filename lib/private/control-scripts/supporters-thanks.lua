f82de26da9ae7a65e00fd1eb78821fdf3176a109
Linver23 <gabrielecavallaro@hotmail.com>
2020-04-13 03:15:23 +0200

    Improved supporter thanks print for be better visible in chat

--- lib/private/control-scripts/supporters-thanks.lua
+++ lib/private/control-scripts/supporters-thanks.lua
@@ -5,7 +5,7 @@ local function createThanksList()
 	for i, supporter in pairs(patreons_list.getHighSupportersList()) do
 		list = list .. "[font=default-large-semibold]" .. 
 		       supporter[1] ..  
-			   " with " .. "[color=green]$" .. supporter[2] .. "[/color]" .. " for month [/font]\n"
+			   " with " .. "[color=green]$" .. supporter[2] .. "[/color]" .. " for month [/font], "
 	end
 	return list
 end
@@ -16,7 +16,7 @@ local function thanksOnPlayerJoin(event)
 		"[font=heading-1]Welcome back to Krastorio![/font] [img=virtual-signal/kr-krastorio]\n" ..
 		"[font=heading-2]This mod is developing thanks to our subscribers!\nAnd we would like to thank these people who supported us on Patreon more than other:[/font]\n" ..
 		createThanksList() .. 
-		"[font=heading-2]and thanks to all others that support us, thank you all![/font]\n\n",
+		"\n[font=heading-2]and thanks to all others that support us, thank you all![/font]\n\n",
 		{1.0, 1.0, 0.2, 1.0}
 	)
 	game.players[event.player_index].print
