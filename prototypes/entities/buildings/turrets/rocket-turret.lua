231f6e13f98dde87b69a970d100871f0b900b0c6
Krastor <rick.ronor@gmail.com>
2020-04-22 08:59:56 +0300

    Turret upd

--- prototypes/entities/buildings/turrets/rocket-turret.lua
+++ prototypes/entities/buildings/turrets/rocket-turret.lua
@@ -1,35 +1,88 @@
 function rocket_turret_sheet(inputs)
-	return
+return
+{
+	layers =
 	{
-		layers = 
 		{
+			filename = kr_entities_path .. "turrets/rocket-turret-sheet.png",
+			width = 220,
+			height = 190,
+			line_length = inputs.frame_count or 8,
+			frame_count = inputs.frame_count or 1,
+			axially_symmetrical = false,
+			direction_count = 64,
+			shift = {0, -0.7},
+			hr_version =
 			{
-				filename = kr_entities_path .. "turrets/rocket-turret-sheet.png",
-				priority = "medium",
-				width = 175,
-				height = 175,
-				direction_count = inputs.direction_count and inputs.direction_count or 64,
-				frame_count = 1,
-				line_length = inputs.line_length and inputs.line_length or 8,
+				filename = kr_entities_path .. "turrets/hr-rocket-turret-sheet.png",
+				width = 440,
+				height = 380,
+				line_length = inputs.frame_count or 8,
+				frame_count = inputs.frame_count or 1,
 				axially_symmetrical = false,
-				run_mode = inputs.run_mode and inputs.run_mode or "forward",
-				hr_version =
-				{
-					filename = kr_entities_path .. "turrets/hr-rocket-turret-sheet.png",
-					priority = "medium",
-					scale = 0.5,
-					width = 350,
-					height = 350,
-					direction_count = inputs.direction_count and inputs.direction_count or 64,
-					frame_count = 1,
-					line_length = inputs.line_length and inputs.line_length or 8,
-					axially_symmetrical = false,
-					run_mode = inputs.run_mode and inputs.run_mode or "forward"
-				}
+				direction_count = 64,
+				shift = {0, -0.7},
+				scale = 0.5
+			}
+		},
+		
+		{
+			flags = { "mask" },
+			filename = kr_entities_path .. "turrets/rocket-turret-sheet-mask.png",
+			width = 220,
+			height = 190,
+			line_length = inputs.frame_count or 8,
+			frame_count = inputs.frame_count or 1,
+			axially_symmetrical = false,
+			direction_count = 64,
+			shift = {0, -0.7},
+			apply_runtime_tint = true,
+			hr_version =
+			{
+				flags = { "mask" },
+				filename = kr_entities_path .. "turrets/hr-rocket-turret-sheet-mask.png",
+				width = 440,
+				height = 380,
+				line_length = inputs.frame_count or 8,
+				frame_count = inputs.frame_count or 1,
+				axially_symmetrical = false,
+				direction_count = 64,
+				shift = {0, -0.7},
+				apply_runtime_tint = true,
+				scale = 0.5
+			}
+		},
+		
+		{
+			filename = kr_entities_path .. "turrets/rocket-turret-sheet-shadow.png",
+			width = 220,
+			height = 190,
+			line_length = inputs.frame_count or 8,
+			frame_count = inputs.frame_count or 1,
+			axially_symmetrical = false,
+			direction_count = 64,
+			shift = {0.9, 0.05},
+			draw_as_shadow = true,
+			hr_version =
+			{
+				filename = kr_entities_path .. "turrets/hr-rocket-turret-sheet-shadow.png",
+				width = 440,
+				height = 380,
+				line_length = inputs.frame_count or 8,
+				frame_count = inputs.frame_count or 1,
+				axially_symmetrical = false,
+				direction_count = 64,
+				shift = {0.9, 0.05},
+				draw_as_shadow = true,
+				scale = 0.5
 			}
 		}
+		
+	
 	}
+}
 end
+
 
 data:extend(
 {  
@@ -56,12 +109,90 @@ data:extend(
 		dying_explosion = "big-explosion",
 		inventory_size = 1,
 		automated_ammo_count = 10,
-		attacking_speed = 0.5,
+		--attacking_speed = 0.5,
 		folded_animation = rocket_turret_sheet{direction_count = 8, line_length = 1},
 		--preparing_animation = rocket_turret_sheet{direction_count = 8, line_length = 1},
 		--prepared_animation = rocket_turret_sheet{},
-		attacking_animation = rocket_turret_sheet{},
+		--attacking_animation = rocket_turret_sheet{direction_count = 8, line_length = 1},
 		--folding_animation = rocket_turret_sheet{direction_count = 8, line_length = 1, run_mode = "backward"},
+		base_picture =
+		{
+			layers =
+			{
+				{
+					filename = kr_entities_path .. "turrets/kr-turret-base.png",
+					priority = "high",
+					width = 220,
+					height = 190,
+					axially_symmetrical = false,
+					direction_count = 1,
+					frame_count = 1,
+					shift = {0, -0.8},
+					hr_version =
+					{
+						filename = kr_entities_path .. "turrets/hr-kr-turret-base.png",
+						priority = "high",
+						width = 440,
+						height = 380,
+						axially_symmetrical = false,
+						direction_count = 1,
+						frame_count = 1,
+						shift = {0, -0.8},
+						scale = 0.5
+					}
+				},	
+				{
+					filename = kr_entities_path .. "turrets/kr-turret-base-mask.png",
+					flags = { "mask", "low-object" },
+					line_length = 1,
+					width = 220,
+					height = 190,
+					axially_symmetrical = false,
+					direction_count = 1,
+					frame_count = 1,
+					shift = {0, -0.8},
+					apply_runtime_tint = true,
+					hr_version =
+					{
+						filename = kr_entities_path .. "turrets/hr-kr-turret-base-mask.png",
+						flags = { "mask", "low-object" },
+						line_length = 1,
+						width = 440,
+						height = 380,
+						axially_symmetrical = false,
+						direction_count = 1,
+						frame_count = 1,
+						shift = {0, -0.8},
+						apply_runtime_tint = true,
+						scale = 0.5
+					}
+				},
+				{
+					filename = kr_entities_path .. "turrets/kr-turret-base-shadow.png",
+					line_length = 1,
+					width = 220,
+					height = 190,
+					axially_symmetrical = false,
+					draw_as_shadow = true,
+					direction_count = 1,
+					frame_count = 1,
+					shift = {0, -0.8},
+					hr_version =
+					{
+						filename = kr_entities_path .. "turrets/hr-kr-turret-base-shadow.png",
+						line_length = 1,
+						width = 440,
+						height = 380,
+						axially_symmetrical = false,
+						draw_as_shadow = true,
+						direction_count = 1,
+						frame_count = 1,
+						shift = {0, -0.8},
+						scale = 0.5
+					}
+				}
+			}
+		},
 		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
 		attack_parameters =
 		{
@@ -69,19 +200,8 @@ data:extend(
 			ammo_category = "missiles-for-turrets",
 			cooldown = 120,
 			turn_range = 1/7,
-			projectile_creation_distance = 2.65,
-			projectile_center = {0, 0.4},
-			shell_particle =
-			{
-				name = "shell-particle",
-				direction_deviation = 0.1,
-				speed = 0.1,
-				speed_deviation = 0.02,
-				center = {0, 0},
-				creation_distance = -3.5,
-				starting_frame_speed = 0.3,
-				starting_frame_speed_deviation = 0.1
-			},
+			projectile_creation_distance = 3.35,
+			projectile_center = {0, 0.1},
 			range = 80,
 			min_range = 35,
 			sound =
@@ -96,6 +216,6 @@ data:extend(
 		turret_base_has_direction = true,
 		open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.75 },
 		close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
-		call_for_help_radius = 85
+		call_for_help_radius = 90
 	}
 })
