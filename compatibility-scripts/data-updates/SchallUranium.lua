6f9badb1ab292aaa68191878b8b4c84b2f8ddc8e
Linver23 <gabrielecavallaro@hotmail.com>
2020-05-04 21:39:21 +0200

    Fixes

--- compatibility-scripts/data-updates/SchallUranium.lua
+++ compatibility-scripts/data-updates/SchallUranium.lua
@@ -1,4 +1,5 @@
 if mods["SchallUraniumProcessing"] then
 	krastorio.recipes.removeProduct("uranium-processing", "uranium-235")
 	krastorio.recipes.removeProduct("uranium-processing", "uranium-238")
+	krastorio.recipes.multiplyIngredient("uranium-fuel-cell", "uranium-low-enriched", 2)
 end
