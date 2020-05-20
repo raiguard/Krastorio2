37d3e552196a20f8d79d76bbf80bc6c658907bff
Linver23 <gabrielecavallaro@hotmail.com>
2020-05-21 01:06:58 +0200

    Fix bug in prototype options

--- lib/private/data-stages/technologies.lua
+++ lib/private/data-stages/technologies.lua
@@ -711,64 +711,65 @@ function krastorio.technologies.sanitizeUnitsOfAllTechnologiesInPacks(science_pa
 			
 			local vanilla_science_pack_collections = krastorio.technologies.science_pack_collections["Vanilla"]
 			for technology_name, technology in pairs(data.raw.technology) do
-				
-				local is_in = false
-				local ingredients = technology.unit.ingredients				
-				if ingredients and next(ingredients) ~= nil then				
-					for i = 1, #ingredients do
-						local ingredient_name = krastorio.technologies.getIngredientName(ingredients[i])
-						for _, science_pack_name in pairs(main_science_pack_collection) do
-							if science_pack_name == ingredient_name then
-								is_in = true
-								break
-							end	
-						end						
-					end					
-					if is_in then
-						local is_sanitized = false
-						local wrong_one = nil
-						local is_wrong = nil
-						while not is_sanitized do
-							wrong_one = -1
-							for i = 1, #ingredients do
-								is_wrong = true
-								local ingredient_name = krastorio.technologies.getIngredientName(ingredients[i])								
-								for _, science_pack_name in pairs(main_science_pack_collection) do
-									if science_pack_name == ingredient_name then
-										is_wrong = false
-										break
-									end	
-								end									
-								if with_vanilla == true and is_wrong == true then
-									for _, science_pack_name in pairs(vanilla_science_pack_collections) do
+				if technology.check_science_packs_incompatibilities ~= false then
+					local is_in = false
+					local ingredients = technology.unit.ingredients				
+					if ingredients and next(ingredients) ~= nil then				
+						for i = 1, #ingredients do
+							local ingredient_name = krastorio.technologies.getIngredientName(ingredients[i])
+							for _, science_pack_name in pairs(main_science_pack_collection) do
+								if science_pack_name == ingredient_name then
+									is_in = true
+									break
+								end	
+							end						
+						end					
+						if is_in then
+							local is_sanitized = false
+							local wrong_one = nil
+							local is_wrong = nil
+							while not is_sanitized do
+								wrong_one = -1
+								for i = 1, #ingredients do
+									is_wrong = true
+									local ingredient_name = krastorio.technologies.getIngredientName(ingredients[i])								
+									for _, science_pack_name in pairs(main_science_pack_collection) do
 										if science_pack_name == ingredient_name then
 											is_wrong = false
 											break
 										end	
-									end
-								end
-								if is_wrong then
-									wrong_one = i
-									break
-								end									
-							end
-							
-							if wrong_one == -1 then
-								is_sanitized = true
-							else
-								for j, prerequisite_name in pairs(technology.prerequisites) do
-									for _, value in pairs(ingredients[wrong_one]) do
-										if prerequisite_name == value then
-											table.remove(technology.prerequisites, j)
-											break
+									end									
+									if with_vanilla == true and is_wrong == true then
+										for _, science_pack_name in pairs(vanilla_science_pack_collections) do
+											if science_pack_name == ingredient_name then
+												is_wrong = false
+												break
+											end	
 										end
-									end								
+									end
+									if is_wrong then
+										wrong_one = i
+										break
+									end									
 								end
-								table.remove(ingredients, wrong_one)								
-							end							
-						end
-					end					
-				end			
+								
+								if wrong_one == -1 then
+									is_sanitized = true
+								else
+									for j, prerequisite_name in pairs(technology.prerequisites) do
+										for _, value in pairs(ingredients[wrong_one]) do
+											if prerequisite_name == value then
+												table.remove(technology.prerequisites, j)
+												break
+											end
+										end								
+									end
+									table.remove(ingredients, wrong_one)								
+								end							
+							end
+						end					
+					end		
+				end
 			end
 		end
 	end
