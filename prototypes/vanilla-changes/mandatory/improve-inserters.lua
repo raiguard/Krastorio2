f3e06e80532d093add20c533a5272651bc3df23b
Linver23 <gabrielecavallaro@hotmail.com>
2020-05-27 10:11:27 +0200

    Fix filter slots bug

--- prototypes/vanilla-changes/mandatory/improve-inserters.lua
+++ prototypes/vanilla-changes/mandatory/improve-inserters.lua
@@ -11,3 +11,10 @@ for _, inserter in pairs(data.raw.inserter) do
 		inserter.localised_description = {"other.additional-burner-inserter-description", inserter.localised_description}
 	end
 end
+
+-- Fix filter inserters bug
+for name, inserter in pairs(data.raw.inserter) do	
+	if inserter.filter_count ~= nil then
+		inserter.filter_count = 4
+	end
+end
