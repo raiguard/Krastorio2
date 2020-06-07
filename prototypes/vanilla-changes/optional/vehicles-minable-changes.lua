a547b32a5ca6a5bd1d3b6a2ba3f3ed06bb813e3f
Linver23 <gabrielecavallaro@hotmail.com>
2020-06-07 06:14:46 +0200

    Options improvement

--- prototypes/vanilla-changes/optional/vehicles-minable-changes.lua
+++ prototypes/vanilla-changes/optional/vehicles-minable-changes.lua
@@ -1,15 +1,9 @@
 -- -- -- Vehicles minable changes
 if krastorio.general.getSafeSettingValue("kr-vehicles-minable-changes") then
-
-	data.raw.car["car"].minable = nil
-	data.raw.car["tank"].minable = nil
-	data.raw.car["kr-advanced-tank"].minable = nil
-	
-	data.raw.locomotive["locomotive"].minable = nil
-	data.raw.locomotive["kr-nuclear-locomotive"].minable = nil
-	data.raw["cargo-wagon"]["cargo-wagon"].minable = nil
-	data.raw["fluid-wagon"]["fluid-wagon"].minable = nil
-	
+	for _, type in ipairs({ "car", "locomotive", "artillery-wagon", "cargo-wagon", "fluid-wagon", }) do
+		for _, vehicle in pairs(data.raw[type]) do
+			vehicle.minable = nil
+		end
+	end
 end
-
 	
