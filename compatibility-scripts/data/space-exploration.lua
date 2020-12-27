f2684e3e98dc4cf3d96aad4d660ab0ea799c5787
raiguard <caleb.heuer@gmail.com>
2020-12-27 13:52:05 -0700

    Make advanced beacons buildable in space

--- compatibility-scripts/data/space-exploration.lua
+++ compatibility-scripts/data/space-exploration.lua
@@ -5,57 +5,58 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 	if se_allow_in_space == nil then
 		se_allow_in_space = {}
 	end
-	
+
 	-- Whitelisting the K2 entities in right way how SE manage the space entities,
 	-- all entities in the se_allow_in_space CAN(POSITIVE) BE placed in the space
 	se_allow_in_space["kr-fuel-refinery"]                     = true
-	
+
 	se_allow_in_space["kr-medium-container"]                  = true
 	se_allow_in_space["kr-medium-active-provider-container"]  = true
 	se_allow_in_space["kr-medium-buffer-container"]           = true
-	se_allow_in_space["kr-medium-passive-provider-container"] = true	
+	se_allow_in_space["kr-medium-passive-provider-container"] = true
 	se_allow_in_space["kr-medium-requester-container"]        = true
 	se_allow_in_space["kr-medium-storage-container"]          = true
-	
+
 	se_allow_in_space["kr-big-container"]                     = true
 	se_allow_in_space["kr-big-active-provider-container"]     = true
 	se_allow_in_space["kr-big-buffer-container"]              = true
 	se_allow_in_space["kr-big-passive-provider-container"]    = true
 	se_allow_in_space["kr-big-requester-container"]           = true
 	se_allow_in_space["kr-big-storage-container"]             = true
-	
+
 	se_allow_in_space["kr-antimatter-reactor"]                = true
+	se_allow_in_space["kr-singularity-beacon"]                = true
 	se_allow_in_space["kr-singularity-lab"]                   = true
-	
+
 	se_allow_in_space["kr-quarry-drill"]                      = true
-	
+
 	se_allow_in_space["fluid-storage-1"]                      = true
 	se_allow_in_space["fluid-storage-2"]                      = true
-	
+
 	se_allow_in_space["kr-sentinel"]                          = true
-	
+
 	se_allow_in_space["kr-advanced-solar-panel"]              = true
-	
+
 	se_allow_in_space["kr-energy-storage"]                    = true
 	se_allow_in_space["kr-intergalactic-transceiver"]         = true
 
 	se_allow_in_space["kr-superior-inserter"]                 = true
 	se_allow_in_space["kr-superior-long-inserter"]            = true
 	se_allow_in_space["kr-superior-filter-inserter"]          = true
-	se_allow_in_space["kr-superior-long-filter-inserter"]     = true	
 	se_allow_in_space["kr-superior-long-filter-inserter"]     = true
-	
+	se_allow_in_space["kr-superior-long-filter-inserter"]     = true
+
 	se_allow_in_space["kr-se-loader"]                         = true
-	
+
 	-- Adding space collisions (Disabling some Krastorio 2 entities in space),
 	-- this is a BLACKLIST, all entities in this table CAN'T(NOT) be placed in the space.
 	local krastorio_entities_to_add =
-	{	
+	{
 		["assembling-machine"] =
 		{
-			"kr-advanced-furnace",			
+			"kr-advanced-furnace",
 			"kr-electrolysis-plant",
-			"kr-filtration-plant",				
+			"kr-filtration-plant",
 			"kr-air-filter"
 		},
 		["boiler"] =
@@ -92,18 +93,18 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 			"kr-wind-turbine"
 		}
 	}
-	
+
 	for category_name, entities in pairs(krastorio_entities_to_add) do
 		for _, entity_name in pairs(entities) do
 			krastorio.entities.addCollisionMaskOnEntity(category_name, entity_name, space_collision_layer or "layer-14")
 		end
 	end
 	------------------------------------------------------------------------------------------------
-	-- Mark tech that are touched by SE	
+	-- Mark tech that are touched by SE
 	function string.starts_with(string_to_check, start_string)
 		return string.sub(string_to_check, 1, string.len(start_string)) == start_string
 	end
-	
+
 	for technology_name, technology in pairs(data.raw.technology) do
 		if string.starts_with(technology_name, "se-") then
 			technology.check_science_packs_incompatibilities = false
