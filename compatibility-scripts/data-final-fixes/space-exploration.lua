5272738d5c8f1059576a06c1a1fb3a47692e2cb7
raiguard <caleb.heuer@gmail.com>
2020-12-27 22:34:51 -0700

    Remove SE 0.2.x compat require

--- compatibility-scripts/data-final-fixes/space-exploration.lua
+++ compatibility-scripts/data-final-fixes/space-exploration.lua
@@ -1,2 +1,1 @@
-require("__Krastorio2__/compatibility-scripts/data-final-fixes/space-exploration/0.2.X/base")
 require("__Krastorio2__/compatibility-scripts/data-final-fixes/space-exploration/0.3.X/base")
