fcffd0182d70b637ae29e9cd29b3a14b1f7c8776
raiguard <caleb.heuer@gmail.com>
2020-12-27 22:19:18 -0700

    Remove 0.2.x space exploration compat scripts

--- compatibility-scripts/data-final-fixes/space-exploration/0.3.X/entities.lua
+++ compatibility-scripts/data-final-fixes/space-exploration/0.3.X/entities.lua
@@ -3,7 +3,7 @@ local kr_color_lib = require(kr_path .. "lib/private/data-stages/colorRGB")
 if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mods["space-exploration"], "0.3.0") then
 	-- Chaning the name of the antimatter reactor for not be the same of the antimatter reactor of SE
 	data.raw["burner-generator"]["kr-antimatter-reactor"].localised_name = {"entity-name.kr-singularity-reactor"}
-	
+
 	-- Remove the SE fuel refiny to not be redundant
 	krastorio.technologies.convertPrerequisiteFromAllTechnologies("se-fuel-refining", "kr-fuel", true)
 	data.raw.technology["se-fuel-refining"] = nil
@@ -12,23 +12,23 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 	krastorio.technologies.addUnlockRecipe("kr-fuel", "solid-fuel-from-heavy-oil")
 	-- Adding to Krastorio fuel refinery the SE fuel refinery crafting category
 	krastorio.entities.addCraftingCategory("assembling-machine", "kr-fuel-refinery", "fuel-refining")
-	
+
 	-- Fix double radars techs
 	krastorio.technologies.convertPrerequisiteFromAllTechnologies("radar", "kr-radar", true)
 	krastorio.technologies.addUnlockRecipe("kr-radar", "radar")
 	data.raw.technology["radar"] = nil
-	
+
 	-- Fix laboratory science packs
 	if data.raw.lab["kr-singularity-lab"] then
 		table.insert(data.raw.lab["kr-singularity-lab"].inputs, "space-science-pack")
 		table.insert(data.raw.lab["kr-singularity-lab"].inputs, "se-deep-space-science-pack")
 	end
-	
+
 	-- Rebalance the electric boiler to not create infinite energy
 	if krastorio.general.getSafeSettingValue("se-electric-boiler") then
 		data.raw["boiler"]["se-electric-boiler"].target_temperature = 99
 	end
-	
+
 	-- Adding new space loader if krastorio 2 loaders are enabled
 	if krastorio.general.getSafeSettingValue("kr-loaders") then
 		local kr_se_graphic_path = kr_graphic_mod_path .. "compatibility/space-exploration/"
@@ -43,36 +43,36 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 				subgroup = "belt"
 			},
 			createKrastorioLoader
-			{				
-				name = "kr-se-loader", 
+			{
+				name = "kr-se-loader",
 				speed =	data.raw["transport-belt"]["se-space-transport-belt"].speed,
 				belt_animation_set = data.raw["transport-belt"]["se-space-transport-belt"].belt_animation_set,
-				order = "z-l[loader]-z[loader]", 
+				order = "z-l[loader]-z[loader]",
 				icon = kr_se_graphic_path .. "kr-se-loader.png",
 				icon_size = 32,
 				tint = kr_color_lib.convert(240, 240, 240, 125),
 				apply_rust = false
-			},	
+			},
 			kr_loader_recipe
 			{
 				name = "kr-se-loader",
-				ingredients = 
+				ingredients =
 				{
 					{"steel-gear-wheel", 10},
 					{"kr-fast-loader", 2},
-					{"se-space-transport-belt", 1}					
+					{"se-space-transport-belt", 1}
 				},
 				subgroup = "belt"
-			}					
+			}
 		})
 		krastorio.technologies.addUnlockRecipe("se-space-platform-scaffold", "kr-se-loader")
 	end
-	
+
 	-- -- Singularity laboratory fixes
 	-- Special collision
 	data.raw["lab"]["kr-singularity-lab"].collision_mask = data.raw["lab"]["se-space-science-lab"].collision_mask
 	krastorio.entities.addLabInput("kr-singularity-lab", "se-deep-space-science-pack")
-	
+
 	-- SPACE COLLISIONS
 	-- After SE have done his magic, because SE don't iterate all entity types,
 	-- to be sure that all entities have the right collision layer,
@@ -85,16 +85,12 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 		{
 			"kr-fuel-refinery"
 		},
-		["beacon"] =
-		{
-			"kr-singularity-beacon"
-		},
 		-- normal containers
 		["container"] =
 		{
 			-- medium storehouse
 			"kr-medium-container",
-			-- big warehouse	
+			-- big warehouse
 			"kr-big-container"
 		},
 		["logistic-container"] =
@@ -115,11 +111,11 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 		["generator"] =
 		{
 			"kr-antimatter-reactor"
-		},	
+		},
 		["mining-drill"] =
 		{
 			"kr-quarry-drill"
-		},	
+		},
 		["storage-tank"] =
 		{
 			"fluid-storage-1",
@@ -150,7 +146,7 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 			"kr-tesla-coil"
 		}
 	}
-	
+
 	for category_name, entities in pairs(krastorio_entities_to_remove) do
 		for _, entity_name in pairs(entities) do
 			krastorio.entities.removeCollisionMaskOnEntity(category_name, entity_name, space_collision_layer)
--- compatibility-scripts/data-final-fixes/space-exploration/0.3.X/items.lua
+++ compatibility-scripts/data-final-fixes/space-exploration/0.3.X/items.lua
@@ -2,7 +2,7 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 	-- Chaning the name of the antimatter reactor fuel for not be the same of the antimatter reactor of SE
 	data.raw.item["empty-antimatter-fuel-cell"].localised_name = {"item-name.empty-singularity-fuel-cell"}
 	data.raw.item["charged-antimatter-fuel-cell"].localised_name = {"item-name.charged-singularity-fuel-cell"}
-	
+
 	-- Rebalancing the se-liquid-rocket-fuel value to be used from the gas power station
 	data.raw.fluid["se-liquid-rocket-fuel"].fuel_value = "1000KJ"
 	data.raw.fluid["se-liquid-rocket-fuel"].fuel_category = "burnable-fluid-fuel"
@@ -10,23 +10,23 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
 	data.raw.fluid["se-liquid-rocket-fuel"].default_temperature = 25
 	data.raw.fluid["se-liquid-rocket-fuel"].max_temperature = 100
 	data.raw.fluid["se-liquid-rocket-fuel"].auto_barrel = true
-	
+
 	-- Belts items re-ordering
 	data.raw.item["kr-advanced-splitter"].subgroup = "splitter"
 	data.raw.item["kr-advanced-transport-belt"].subgroup = "transport-belt"
 	data.raw.item["kr-advanced-underground-belt"].subgroup = "underground-belt"
-	
+
 	data.raw.item["kr-superior-splitter"].subgroup = "splitter"
 	data.raw.item["kr-superior-transport-belt"].subgroup = "transport-belt"
 	data.raw.item["kr-superior-underground-belt"].subgroup = "underground-belt"
-	
+
 	-- Labs ordering
 	data.raw.item["biusart-lab"].subgroup = "lab"
 	data.raw.item["kr-singularity-lab"].subgroup = "lab"
-	
+
 	data.raw.item["se-space-science-lab"].order = "g[lab]-g3[kr-singularity-lab]"
 	data.raw.item["kr-singularity-lab"].order = "g[lab]-g4[kr-singularity-lab]"
-	
+
 	if data.raw.capsule["first-aid-kit"] then
 		data.raw.capsule["first-aid-kit"].capsule_action.attack_parameters.ammo_type.action.action_delivery.target_effects.damage = {type = "physical", amount = -50}
 	end
