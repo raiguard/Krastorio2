3aec54906f0249ab2bd6f920d763ddf3debe5b66
raiguard <caleb.heuer@gmail.com>
2021-03-06 22:06:28 -0700

    Remove other flib artifacts

--- settings.lua
+++ settings.lua
@@ -278,16 +278,3 @@ data:extend(
 		order = "b16"
 	}
 })
-
-if not mods["flib"] then
-	data:extend(
-	{
-		{
-			type = "int-setting",
-			name = "flib-translations-per-tick",
-			setting_type = "runtime-global",
-			default_value = 50,
-			minimum_value = 1
-		}
-	})
-end
