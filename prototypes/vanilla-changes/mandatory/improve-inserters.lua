6cb7ebc434400f8df061247e3f271269c9210c1e
raiguard <caleb.heuer@gmail.com>
2021-05-17 11:00:47 -0600

    Don't show inserter tooltips with bobinserters

--- prototypes/vanilla-changes/mandatory/improve-inserters.lua
+++ prototypes/vanilla-changes/mandatory/improve-inserters.lua
@@ -1,19 +1,27 @@
 -- Enabling custom vectors and burner leech (if possible) on all inserters
 for _, inserter in pairs(data.raw.inserter) do
-  inserter.allow_custom_vectors = true  
-  if inserter.localized_description then
-    inserter.localised_description = { "other.conservative-additional-inserter-description", inserter.localised_description, {"other.additional-inserter-description"} }
-  else
-    inserter.localised_description = {"other.additional-inserter-description"}
+  inserter.allow_custom_vectors = true
+  if not mods["bobinserters"] then
+    if inserter.localized_description then
+      inserter.localised_description = {
+        "other.conservative-additional-inserter-description",
+        inserter.localised_description,
+        {"other.additional-inserter-description"}
+      }
+    else
+      inserter.localised_description = {"other.additional-inserter-description"}
+    end
   end
   if inserter.energy_source and inserter.energy_source.type == "burner" then
-    inserter.allow_burner_leech = true    
-    inserter.localised_description = {"other.additional-burner-inserter-description", inserter.localised_description}
+    inserter.allow_burner_leech = true
+    if not mods["bobinserters"] then
+      inserter.localised_description = {"other.additional-burner-inserter-description", inserter.localised_description}
+    end
   end
 end
 
 -- Fix filter inserters bug
-for name, inserter in pairs(data.raw.inserter) do 
+for name, inserter in pairs(data.raw.inserter) do
   if inserter.filter_count ~= nil then
     inserter.filter_count = 4
   end
