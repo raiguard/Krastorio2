a9ffeada40b8a8a76587a6f83ca63fdbc4d89bf2
raiguard <caleb.heuer@gmail.com>
2021-07-11 01:46:35 -0600

    Put entities in a subtable

--- scripts/entity/tesla-coil.lua
+++ scripts/entity/tesla-coil.lua
@@ -9,19 +9,21 @@ function tesla_coil.build(source_entity)
   local unit_number = source_entity.unit_number
 
   global.tesla_coils[unit_number] = {
-    collision = surface.create_entity {
-      name = "kr-tesla-coil-collision",
-      position = source_entity.position,
-      force = source_entity.force,
-      create_build_effect_smoke = false,
+    entities = {
+      collision = surface.create_entity {
+        name = "kr-tesla-coil-collision",
+        position = source_entity.position,
+        force = source_entity.force,
+        create_build_effect_smoke = false,
+      },
+      land_mine = surface.create_entity{
+        name = "kr-tesla-coil-land-mine",
+        position = source_entity.position,
+        force = source_entity.force,
+        create_build_effect_smoke = false,
+      },
+      tower = source_entity,
     },
-    land_mine = surface.create_entity{
-      name = "kr-tesla-coil-land-mine",
-      position = source_entity.position,
-      force = source_entity.force,
-      create_build_effect_smoke = false,
-    },
-    tower = source_entity,
   }
 end
 
@@ -29,7 +31,7 @@ function tesla_coil.destroy(source_entity)
   local unit_number = source_entity.unit_number
   local data = global.tesla_coils[unit_number]
   if data then
-    for _, entity in pairs(data) do
+    for _, entity in pairs(data.entities) do
       if entity and entity.valid then
         entity.destroy()
       end
