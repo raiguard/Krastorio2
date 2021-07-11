7048fc397a1e0e80d4f76059b399a033f499d78e
raiguard <caleb.heuer@gmail.com>
2021-07-10 20:21:04 -0600

    Add migrations from previous versions

--- scripts/migrations.lua
+++ scripts/migrations.lua
@@ -1,1 +1,35 @@
-return {}
+local tesla_coil = require("scripts.entity.tesla-coil")
+
+return {
+  ["1.1.2"] = function()
+    local shelters = global.spawn_points
+    local filters = { name = { "kr-shelter-light", "kr-shelter-plus-light" } }
+    if shelters then
+      for _, surface in pairs(game.surfaces) do
+        for _, light in pairs(surface.find_entities_filtered(filters)) do
+          local force = light.force.index
+          local surface_data = shelters[surface.index]
+          if surface_data then
+            local shelter_data = surface_data[force]
+            if
+              shelter_data
+              and surface.find_entities_filtered({
+                name = { "kr-shelter-container", "kr-shelter-plus-container" },
+                position = light.position,
+              })[1]
+            then
+              shelter_data[#shelter_data + 1] = light
+            else
+              light.destroy()
+            end
+          else
+            light.destroy()
+          end
+        end
+      end
+    end
+  end,
+  ["1.2.0"] = function()
+    -- TODO: Add collision entities and data for tesla coils
+  end
+}
