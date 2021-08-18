e305378938f42cb408cb378e934210d33ef2d20e
raiguard <caleb.heuer@gmail.com>
2021-08-17 23:11:00 -0600

    creep_settings -> creep

--- scripts/creep.lua
+++ scripts/creep.lua
@@ -6,7 +6,7 @@ local creep = {}
 local function generate_creep(entities)
   -- Check if this surface is allowed to generate creep
   local surface = entities[1].surface
-  if not global.creep_settings.surfaces[surface.index] then return end
+  if not global.creep.surfaces[surface.index] then return end
 
   local radius = math.random(3, 5)
   local to_add = {}
@@ -21,7 +21,7 @@ local function generate_creep(entities)
 end
 
 function creep.init()
-  global.creep_settings = {
+  global.creep = {
     on_biter_base_built = true,
     on_chunk_generated = true,
     surfaces = {[game.get_surface("nauvis").index] = true},
@@ -29,13 +29,13 @@ function creep.init()
 end
 
 function creep.on_biter_base_built(entity)
-  if entity.type == "unit-spawner" and global.creep_settings.surfaces[entity.surface.index] then
+  if entity.type == "unit-spawner" and global.creep.surfaces[entity.surface.index] then
     generate_creep{entity}
   end
 end
 
 function creep.on_chunk_generated(chunk_area, surface)
-  if not global.creep_settings.surfaces[surface.index] then return end
+  if not global.creep.surfaces[surface.index] then return end
 
   local entities = surface.find_entities_filtered(
     {type = {"unit-spawner"}, area = chunk_area, force = "enemy"}
@@ -51,13 +51,13 @@ creep.remote_interface = {
     if type(value) ~= "boolean" then
       error("Value for 'creep_on_chunk_generated' must be a boolean.")
     end
-    global.creep_settings.on_chunk_generated = value
+    global.creep.on_chunk_generated = value
   end,
   set_creep_on_biter_base_built = function(value)
     if type(value) ~= "boolean" then
       error("Value for 'creep_on_biter_base_built' must be a boolean.")
     end
-    global.creep_settings.on_biter_base_built = value
+    global.creep.on_biter_base_built = value
   end,
   spawn_creep_at_position = function(surface, position, override)
     if type(surface) ~= "table" or type(position) ~= "table" or not surface.valid then
@@ -65,7 +65,7 @@ creep.remote_interface = {
     end
     -- The code here is duplicated from `generate_creep()` because that function is specifically optimized for multiple
     -- entities, while this function only needs to do it once.
-    if not global.creep_settings.surfaces[surface.index] and not override then return end
+    if not global.creep.surfaces[surface.index] and not override then return end
     local radius = math.random(3, 5)
     surface.set_tiles(
       table.map(
