c66c6ba49938977eef8195ddff0eaff61926c04a
raiguard <caleb.heuer@gmail.com>
2021-08-17 23:46:17 -0600

    Implement biter virus

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -1,6 +1,8 @@
+local math = require("__flib__.math")
+
 local constants = {}
 
-constants.creep_to_remove_per_tick = 300
+constants.biter_virus_evolution_multiplier = 0.67
 
 constants.tesla_coil = {
   charging_rate = 3000000, -- 3 MW
@@ -8,6 +10,17 @@ constants.tesla_coil = {
   loss_multiplier = 1.5,
   required_energy = 10000000, -- 10 MW
   simultaneous_allowed = 4,
+}
+
+constants.virus_iteration_counts = {
+  [1000] = 20,
+  [5000] = 40,
+  [10000] = 80,
+  [20000] = 160,
+  [30000] = 280,
+  [40000] = 300,
+  [50000] = 400,
+  [math.max_int53] = 800,
 }
 
 return constants
--- scripts/virus.lua
+++ scripts/virus.lua
@@ -2,40 +2,109 @@ local constants = require("scripts.constants")
 
 local virus = {}
 
-local function iterate_biter_virus(virus_data)
+-- Modifier to change the quantity of objects iterated for each round of the function, based on the total
+local function get_removal_count(count)
+  for limit, per in pairs(constants.virus_iteration_counts) do
+    if count <= limit then
+      return per
+    end
+  end
+end
 
+local function iterate_biter_virus(virus_data)
+  local surface = virus_data.surface
+  if not surface or not surface.valid then
+    global.virus.biter[surface.index] = nil
+    return
+  end
+
+  local entities = virus_data.entities
+  local entities_killed = virus_data.entities_killed
+  local entities_to_kill = virus_data.entities_to_kill
+  local force = virus_data.force
+  local len = virus_data.entities_len
+
+  -- Kill some entities
+  for _ = 1, virus_data.amount_per_iteration do
+    if entities_killed >= entities_to_kill then
+      global.virus.biter[surface.index] = nil
+      break
+    end
+
+    local i = math.random(1, len)
+    local entity = entities[i]
+    if entity and entity.valid then
+      entity.die(force)
+
+      -- Move the element at the end to the gap
+      -- This removes the element at `i` while achieving O(1) performance
+      entities[i] = entities[len]
+      entities[len] = nil
+
+      entities_killed = entities_killed + 1
+      len = len - 1
+    end
+  end
+
+  virus_data.entities_killed = entities_killed
+  virus_data.tiles_len = len
+end
+
+local function init_biter_virus(player, surface)
+  local biter_viruses = global.virus.biter
+  if not biter_viruses[surface.index] then
+    -- Reduce evolution factor
+    local enemy = game.forces.enemy
+    enemy.evolution_factor = enemy.evolution_factor * constants.biter_virus_evolution_multiplier
+
+    -- Begin gradual enemy killoff
+    local enemy_entities = surface.find_entities_filtered({force = "enemy"})
+    local len = #enemy_entities
+    if len > 0 then
+      biter_viruses[surface.index] = {
+        amount_per_iteration = math.floor(len / get_removal_count(len)),
+        entities = enemy_entities,
+        entities_killed = 0,
+        entities_len = len,
+        entities_to_kill = len / 2.5,
+        force = player.force,
+        surface = surface,
+      }
+    end
+  end
 end
 
 local function iterate_creep_virus(virus_data)
   local surface = virus_data.surface
-  if surface and surface.valid then
-    local creeps = virus_data.tiles
-    local len = virus_data.tiles_len
-    local tiles_to_replace = {}
-    for i = 1, virus_data.amount_per_iteration do
-      if len == 0 then
-        global.virus.creep[surface.index] = nil
-        break
-      end
-      local j = math.random(1, len)
-      local tile = creeps[j]
-      if tile and tile.valid then
-        tiles_to_replace[i] = { name = tile.hidden_tile or "landfill", position = tile.position }
-        -- Move the element at the end to the gap
-        -- This effectively removes the element from the table while retaining an O(1) performance impact
-        creeps[j] = creeps[len]
-        creeps[len] = nil
-        len = len - 1
-      end
-    end
-    virus_data.tiles_len = len
-    surface.set_tiles(tiles_to_replace)
-  else
+  if not surface or not surface.valid then
     global.virus.creep[surface.index] = nil
+    return
   end
+
+  local creeps = virus_data.tiles
+  local len = virus_data.tiles_len
+  local tiles_to_replace = {}
+  for i = 1, virus_data.amount_per_iteration do
+    if len == 0 then
+      global.virus.creep[surface.index] = nil
+      break
+    end
+    local j = math.random(1, len)
+    local tile = creeps[j]
+    if tile and tile.valid then
+      tiles_to_replace[i] = {name = tile.hidden_tile or "landfill", position = tile.position}
+      -- Move the element at the end to the gap
+      -- This removes the element at `i` while achieving O(1) performance
+      creeps[j] = creeps[len]
+      creeps[len] = nil
+      len = len - 1
+    end
+  end
+  virus_data.tiles_len = len
+  surface.set_tiles(tiles_to_replace)
 end
 
-local function init_creep_virus(surface, position)
+local function init_creep_virus(surface)
   local creep_viruses = global.virus.creep
   if not creep_viruses[surface.index] then
       -- Disable creep generation on this surface
@@ -46,7 +115,7 @@ local function init_creep_virus(surface, position)
       local creep_tiles = surface.find_tiles_filtered({name = "kr-creep"})
       local num_creeps = #creep_tiles
       creep_viruses[surface.index] = {
-        amount_per_iteration = math.floor(num_creeps / constants.creep_to_remove_per_tick),
+        amount_per_iteration = math.floor(num_creeps / get_removal_count(num_creeps)),
         surface = surface,
         tiles = creep_tiles,
         tiles_len = num_creeps,
@@ -65,14 +134,13 @@ function virus.on_player_used_capsule(e)
   local item = e.item
   if not item or not item.valid then return end
 
-  local surface = game.get_player(e.player_index).surface
-  local position = e.position
+  local player = game.get_player(e.player_index)
+  local surface = player.surface
 
   if item.name == "kr-biter-virus" then
-    game.print("BITER VIRUS")
+    init_biter_virus(player, surface)
   elseif item.name == "kr-creep-virus" then
-    init_creep_virus(surface, position)
-    game.print("CREEP VIRUS")
+    init_creep_virus(surface)
   end
 end
 
