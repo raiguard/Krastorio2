2fb4483649419eb59fe46c3e083ddc4ed82da512
raiguard <caleb.heuer@gmail.com>
2021-08-17 20:03:16 -0600

    Increase tesla coil input flow limit

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -2,7 +2,7 @@ local constants = {}
 
 constants.tesla_coil = {
   charging_rate = 3000000, -- 3 MW
-  input_flow_limit = 15000000, -- 8 MW
+  input_flow_limit = 18000000, -- 8 MW
   loss_multiplier = 1.5,
   required_energy = 10000000, -- 10 MW
   simultaneous_allowed = 4,
