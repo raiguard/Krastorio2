22cab22cc094ec7789cc92daddcf2a5b2800523e
raiguard <caleb.heuer@gmail.com>
2021-08-24 22:45:49 -0600

    Add jackhammer and creep collector shortcuts

---
+++ prototypes/others/shortcut.lua
@@ -0,0 +1,18 @@
+local data_util = require("__flib__.data-util")
+
+data:extend{
+  {
+    type = "shortcut",
+    name = "kr-creep-collector",
+    icon = {filename = data_util.empty_image, size = 1},
+    action = "spawn-item",
+    item_to_spawn = "kr-creep-collector",
+  },
+  {
+    type = "shortcut",
+    name = "kr-jackhammer",
+    icon = {filename = data_util.empty_image, size = 1},
+    action = "spawn-item",
+    item_to_spawn = "kr-jackhammer",
+  },
+}
