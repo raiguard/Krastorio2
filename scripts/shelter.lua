74386a0bfc059e092d10ba49872a35b5fe96f3fe
raiguard <caleb.heuer@gmail.com>
2021-08-25 23:08:51 -0600

    Refactor shelter data structure

--- scripts/shelter.lua
+++ scripts/shelter.lua
@@ -3,7 +3,10 @@ local util = require("scripts.util")
 local shelter = {}
 
 function shelter.init()
-  global.shelter = {}
+  global.shelter = {
+    forces = {},
+    inactive = {},
+  }
 
   for _, force in pairs(game.forces) do
     shelter.force_init(force)
@@ -11,21 +14,16 @@ function shelter.init()
 end
 
 function shelter.force_init(force)
-  global.shelter[force.index] = {
-    inactive_shelters = {},
-    spawn_points = {},
-  }
+  global.shelter.forces[force.index] = {}
 end
 
 function shelter.build(entity)
   local force = entity.force
   local surface = entity.surface
-  local force_data = global.shelter[force.index]
-  if not force_data then return end
-  local spawn_points = force_data.spawn_points
+  local force_shelters = global.shelter.forces[force.index]
 
-  if spawn_points[surface.index] then
-    force_data.inactive_shelters[entity.unit_number] = entity
+  if force_shelters[surface.index] then
+    global.shelter.inactive[entity.unit_number] = entity
     entity.active = false
     entity.operable = false
   else
@@ -50,43 +48,40 @@ function shelter.build(entity)
     shelter_data.former_spawn_point = force.get_spawn_position(surface)
     force.set_spawn_position({x = entity.position.x, y = entity.position.y + 3.5}, surface)
 
-    spawn_points[surface.index] = shelter_data
+    force_shelters[surface.index] = shelter_data
   end
 end
 
 function shelter.destroy(entity)
-  local force = entity.force
-  local force_data = global.shelter[force.index]
+  local inactive = global.shelter.inactive[entity.unit_number]
+  if inactive then
+    global.shelter.inactive[entity.unit_number] = nil
+  else
+    local force = entity.force
 
-  if force_data then
+    local force_shelters = global.shelter.forces[force.index]
+    if not force_shelters then return end
+
     local surface_index = entity.surface.index
-    local inactive = force_data.inactive_shelters[entity.unit_number]
-    if inactive then
-      force_data.inactive_shelters[entity.unit_number] = nil
-    else
-      local spawn_points = force_data.spawn_points
-      local shelter_data = spawn_points[surface_index]
-      if shelter_data then
-        -- Destroy entities
-        shelter_data.electric.destroy()
-        shelter_data.light.destroy()
-        spawn_points[surface_index] = nil
+    local shelter_data = force_shelters[surface_index]
+    if not shelter_data then return end
 
-        -- Restore former spawn point
-        force.set_spawn_position(shelter_data.former_spawn_point, surface_index)
-      end
-    end
+    -- Destroy entities
+    shelter_data.electric.destroy()
+    shelter_data.light.destroy()
+    force_shelters[surface_index] = nil
+
+    -- Restore former spawn point
+    force.set_spawn_position(shelter_data.former_spawn_point, surface_index)
   end
 end
 
 function shelter.spawn_flying_texts()
-  for _, force_data in pairs(global.shelter) do
-    for unit_number, entity in pairs(force_data.inactive_shelters) do
-      if entity.valid then
-        util.entity_flying_text(entity, {"message.kr-shelter-is-inactive"}, {r = 1})
-      else
-        force_data.inactive_shelters[unit_number] = nil
-      end
+  for unit_number, entity in pairs(global.shelter.inactive) do
+    if entity.valid then
+      util.entity_flying_text(entity, {"message.kr-shelter-is-inactive"}, {r = 1})
+    else
+      global.shelter.inactive[unit_number] = nil
     end
   end
 end
