3969b590dbe5fd7068220ead9aedc2907da2403b
raiguard <caleb.heuer@gmail.com>
2021-11-03 18:45:44 -0600

    Remove liquid rocket fuel adjustments (closes #63)

--- compatibility-scripts/data-final-fixes/space-exploration/0.3.X/items.lua
+++ compatibility-scripts/data-final-fixes/space-exploration/0.3.X/items.lua
@@ -3,13 +3,14 @@ if mods["space-exploration"] and krastorio.general.isVersionGreaterEqualThan(mod
   data.raw.item["empty-antimatter-fuel-cell"].localised_name = { "item-name.empty-singularity-fuel-cell" }
   data.raw.item["charged-antimatter-fuel-cell"].localised_name = { "item-name.charged-singularity-fuel-cell" }
 
+  -- Removed as per issue #63
   -- Rebalancing the se-liquid-rocket-fuel value to be used from the gas power station
-  data.raw.fluid["se-liquid-rocket-fuel"].fuel_value = "1000KJ"
-  data.raw.fluid["se-liquid-rocket-fuel"].fuel_category = "burnable-fluid-fuel"
-  data.raw.fluid["se-liquid-rocket-fuel"].fuel_emissions_multiplier = 1.5
-  data.raw.fluid["se-liquid-rocket-fuel"].default_temperature = 25
-  data.raw.fluid["se-liquid-rocket-fuel"].max_temperature = 100
-  data.raw.fluid["se-liquid-rocket-fuel"].auto_barrel = true
+  -- data.raw.fluid["se-liquid-rocket-fuel"].fuel_value = "1000KJ"
+  -- data.raw.fluid["se-liquid-rocket-fuel"].fuel_category = "burnable-fluid-fuel"
+  -- data.raw.fluid["se-liquid-rocket-fuel"].fuel_emissions_multiplier = 1.5
+  -- data.raw.fluid["se-liquid-rocket-fuel"].default_temperature = 25
+  -- data.raw.fluid["se-liquid-rocket-fuel"].max_temperature = 100
+  -- data.raw.fluid["se-liquid-rocket-fuel"].auto_barrel = true
 
   -- Belts items re-ordering
   data.raw.item["kr-advanced-splitter"].subgroup = "splitter"
