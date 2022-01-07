7dd36487dbc69e4f6df14c2d88bb4a00fcb29d8c
raiguard <caleb.heuer@gmail.com>
2022-01-06 17:24:37 -0700

    Check entity validity before generating creep (fixes #126)

--- scripts/creep.lua
+++ scripts/creep.lua
@@ -48,7 +48,9 @@ function creep.on_chunk_generated(chunk_area, surface)
   local entities = surface.find_entities_filtered({ type = { "unit-spawner" }, area = chunk_area, force = "enemy" })
 
   for _, entity in pairs(entities) do
-    generate_creep({ entity })
+    if entity.valid then
+      generate_creep({ entity })
+    end
   end
 end
 
