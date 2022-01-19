b507722956d5c487409e4e6b6cd02e7678e3944f
Patrick 'Quezler' Mounier <Quezler@me.com>
2022-01-19 21:04:43 +0100

    Leave logistic chests alone if `aai-containers` is present

--- prototypes/vanilla-changes/mandatory/items-sorting.lua
+++ prototypes/vanilla-changes/mandatory/items-sorting.lua
@@ -1,17 +1,19 @@
 -- -- -- Sorting changes to vanilla items
 
 -- Logistic chests
-local logistc_subgroup_1 = "kr-logistics-1"
-data.raw.item["logistic-chest-active-provider"].subgroup = logistc_subgroup_1
-data.raw.item["logistic-chest-active-provider"].order = "a[chest-active-provider]"
-data.raw.item["logistic-chest-buffer"].subgroup = logistc_subgroup_1
-data.raw.item["logistic-chest-buffer"].order = "b[chest-buffer]"
-data.raw.item["logistic-chest-passive-provider"].subgroup = logistc_subgroup_1
-data.raw.item["logistic-chest-passive-provider"].order = "c[chest-passive-provider]"
-data.raw.item["logistic-chest-requester"].subgroup = logistc_subgroup_1
-data.raw.item["logistic-chest-requester"].order = "d[chest-requester]"
-data.raw.item["logistic-chest-storage"].subgroup = logistc_subgroup_1
-data.raw.item["logistic-chest-storage"].order = "e[chest-storage]"
+if mods["aai-containers"] then
+  local logistic_subgroup_1 = "kr-logistics-1"
+  data.raw.item["logistic-chest-active-provider"].subgroup = logistic_subgroup_1
+  data.raw.item["logistic-chest-active-provider"].order = "a[chest-active-provider]"
+  data.raw.item["logistic-chest-buffer"].subgroup = logistic_subgroup_1
+  data.raw.item["logistic-chest-buffer"].order = "b[chest-buffer]"
+  data.raw.item["logistic-chest-passive-provider"].subgroup = logistic_subgroup_1
+  data.raw.item["logistic-chest-passive-provider"].order = "c[chest-passive-provider]"
+  data.raw.item["logistic-chest-requester"].subgroup = logistic_subgroup_1
+  data.raw.item["logistic-chest-requester"].order = "d[chest-requester]"
+  data.raw.item["logistic-chest-storage"].subgroup = logistic_subgroup_1
+  data.raw.item["logistic-chest-storage"].order = "e[chest-storage]"
+end
 
 -- Radar and rocket silo
 data.raw.item["radar"].subgroup = "radars-and-rockets"
