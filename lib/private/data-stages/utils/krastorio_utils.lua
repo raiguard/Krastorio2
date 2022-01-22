696b50ec16513cd7214ec6a39b30ae55d7a4b393
raiguard <caleb.heuer@gmail.com>
2022-01-21 20:54:42 -0700

    Remove vanilla_data_backups

--- lib/private/data-stages/utils/krastorio_utils.lua
+++ lib/private/data-stages/utils/krastorio_utils.lua
@@ -1,7 +1,3 @@
 krastorio_utils = {}
 require("private_stdlib.tables")
 require("log")
--- backups
-require(kr_path .. "lib/private/data-stages/utils/vanilla_data_backups/recipes_backup")
-require(kr_path .. "lib/private/data-stages/utils/vanilla_data_backups/recipes_backup_utils")
-require(kr_path .. "lib/private/data-stages/utils/vanilla_data_backups/technologies_backup")
