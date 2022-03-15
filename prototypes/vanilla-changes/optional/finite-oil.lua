86175cc579739a31aa3bb800272102180960331e
raiguard <caleb.heuer@gmail.com>
2022-03-14 21:38:48 -0600

    Add mineral water infinite option (closes #240)

--- prototypes/vanilla-changes/optional/finite-oil.lua
+++ prototypes/vanilla-changes/optional/finite-oil.lua
@@ -34,4 +34,38 @@ if krastorio.general.getSafeSettingValue("kr-finite-oil") then
     has_starting_area_placement = false,
     regular_rq_factor_multiplier = 1,
   })
+else
+  data.raw["mining-drill"]["kr-mineral-water-pumpjack"].energy_usage = "100kW"
+  data.raw["mining-drill"]["kr-mineral-water-pumpjack"].mining_speed = 2
+
+  data.raw.resource["mineral-water"].infinite = true
+  data.raw.resource["mineral-water"].minimum = 60000
+  data.raw.resource["mineral-water"].normal = 300000
+  data.raw.resource["mineral-water"].infinite_depletion_amount = 10
+  data.raw.resource["mineral-water"].minable = {
+    mining_time = 1,
+    results = {
+      {
+        type = "fluid",
+        name = "mineral-water",
+        amount_min = 10,
+        amount_max = 10,
+        probability = 1,
+      },
+    },
+  }
+
+  -- Copy of the crude oil settings from the base mod
+  data.raw.resource["mineral-water"].autoplace = resource_autoplace.resource_autoplace_settings({
+    name = "mineral-water",
+    order = "c", -- Other resources are "b"; oil won't get placed if something else is already there.
+    base_density = 8.2,
+    base_spots_per_km2 = 1.8,
+    random_probability = 1 / 48,
+    random_spot_size_minimum = 1,
+    random_spot_size_maximum = 1, -- don't randomize spot size
+    additional_richness = 220000, -- this increases the total everywhere, so base_density needs to be decreased to compensate
+    has_starting_area_placement = false,
+    regular_rq_factor_multiplier = 1,
+  })
 end
