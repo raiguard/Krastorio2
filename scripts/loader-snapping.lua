3e6a64960c59c2d4c49138132a57fb41e5a1d1c7
raiguard <caleb.heuer@gmail.com>
2022-04-13 19:59:36 -0600

    Don't flip the linked belt if it's connected

--- scripts/loader-snapping.lua
+++ scripts/loader-snapping.lua
@@ -74,6 +74,10 @@ function loader_snapping.snap_belt_neighbours(entity)
       end
     end
     if entity.type == "linked-belt" then
+      -- Trying to "flip" the linked belt when it is connected throws an exception
+      if entity.linked_belt_neighbour then
+        break
+      end
       -- Calling rotate() on a linked belt will actually rotate it instead of changing its type
       entity.linked_belt_type = entity.linked_belt_type == "output" and "input" or "output"
     else
