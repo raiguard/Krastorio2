370b433c5146d27cf89a91f54c61c35f3ef19344
Caleb Heuer <ch@raiguard.me>
2022-11-14 21:27:29 -0700

    Round up roboport ranges to whole tiles

--- lib/public/data-stages/create-roboport-states.lua
+++ lib/public/data-stages/create-roboport-states.lua
@@ -14,7 +14,8 @@ function create_roboport_states.createRoboportVariations(roboport_name)
     logistic_roboport.has_backer_name = false
     logistic_roboport.localised_description = { "entity-description." .. roboport.name }
     logistic_roboport.placeable_by = { item = roboport.name, count = 1 }
-    logistic_roboport.logistics_radius = logistic_roboport.logistics_radius + logistic_roboport.logistics_radius * 0.275 -- Standard roboport has 64x64 radius
+    logistic_roboport.logistics_radius =
+      math.ceil(logistic_roboport.logistics_radius + logistic_roboport.logistics_radius * 0.275) -- Standard roboport has 64x64 radius
     logistic_roboport.construction_radius = 0
     logistic_roboport.fast_replaceable_group = fr_group
 
@@ -28,10 +29,10 @@ function create_roboport_states.createRoboportVariations(roboport_name)
     construction_roboport.has_backer_name = false
     construction_roboport.localised_description = { "entity-description." .. roboport.name }
     construction_roboport.placeable_by = { item = roboport.name, count = 1 }
-    construction_roboport.construction_radius = construction_roboport.construction_radius
-      + construction_roboport.construction_radius * 0.25
+    construction_roboport.construction_radius =
+      math.ceil(construction_roboport.construction_radius + construction_roboport.construction_radius * 0.25)
     construction_roboport.logistics_connection_distance =
-      math.min(construction_roboport.logistics_radius * 1.5, construction_roboport.construction_radius)
+      math.ceil(math.min(construction_roboport.logistics_radius * 1.5, construction_roboport.construction_radius))
     construction_roboport.logistics_radius = 0
     construction_roboport.fast_replaceable_group = fr_group
 
