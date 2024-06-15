8a892769e7f4c080746ec7d230b155ee517ea5e9
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-06-15 14:14:02 -0500

    Update compatibility.lua

--- scripts/compatibility.lua
+++ scripts/compatibility.lua
@@ -36,6 +36,32 @@ function compatibility.schall_uranium()
   end
 end
 
+function compatibility.nuclear_fuel()
+  if script.active_mods["nuclear-overhaul"] then
+    remote.call("kr-radioactivity", "add_entity", "thorium-ore")
+    remote.call("kr-radioactivity", "add_item", "neptunium-237")
+    remote.call("kr-radioactivity", "add_item", "plutonium-238")
+    remote.call("kr-radioactivity", "add_item", "plutonium-239")
+    remote.call("kr-radioactivity", "add_item", "plutonium-fuel")
+    remote.call("kr-radioactivity", "add_item", "plutonium-mox")
+    remote.call("kr-radioactivity", "add_item", "protactinium-233")
+    remote.call("kr-radioactivity", "add_item", "protactinium-234")
+    remote.call("kr-radioactivity", "add_item", "thorium-232")
+    remote.call("kr-radioactivity", "add_item", "thorium-233")
+    remote.call("kr-radioactivity", "add_item", "thorium-fuel")
+    remote.call("kr-radioactivity", "add_item", "thorium-mox")
+    remote.call("kr-radioactivity", "add_item", "thorium-ore")
+    remote.call("kr-radioactivity", "add_item", "uranium-233")
+    remote.call("kr-radioactivity", "add_item", "uranium-234")
+    remote.call("kr-radioactivity", "add_item", "uranium-236")
+    remote.call("kr-radioactivity", "add_item", "uranium-237")
+    remote.call("kr-radioactivity", "add_item", "uranium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-plutonium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-thorium-mox")
+    remote.call("kr-radioactivity", "add_item", "used-uranium-mox")
+  end
+end
+
 function compatibility.should_build_pump()
   if game.entity_prototypes["kr-electric-offshore-pump"] then
     return true
