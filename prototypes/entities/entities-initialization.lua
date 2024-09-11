07faceb455a75b4727394bf5bbc5653fc16f82aa
Caleb Heuer <ch@raiguard.me>
2024-09-11 19:49:28 +0200

    Easily check how many are left

--- prototypes/entities/entities-initialization.lua
+++ prototypes/entities/entities-initialization.lua
@@ -1,7 +1,3 @@
----------------------------------------------------------------------------------------
--- -- -- BUILDINGS
----------------------------------------------------------------------------------------
--- -- Generals
 require("prototypes.entities.intergalactic-transceiver")
 require("prototypes.entities.planetary-teleporter")
 require("prototypes.entities.matter-assembler")
@@ -25,50 +21,36 @@ require("prototypes.entities.advanced-assembling-machine")
 require("prototypes.entities.electric-mining-drills")
 require("prototypes.entities.tesla-coil")
 require("prototypes.entities.logo")
--- -- Turrets
 require("prototypes.entities.railgun-turret")
 require("prototypes.entities.rocket-turret")
 require("prototypes.entities.laser-artillery-turret")
--- -- Laboratories
 require("prototypes.entities.biusart-lab")
 require("prototypes.entities.singularity-lab")
--- -- Roboports
 require("prototypes.entities.small-roboport")
 require("prototypes.entities.large-roboport")
--- -- Conteiners
--- Mediums
 require("prototypes.entities.medium-container")
 require("prototypes.entities.medium-active-provider-container")
 require("prototypes.entities.medium-buffer-container")
 require("prototypes.entities.medium-passive-provider-container")
 require("prototypes.entities.medium-requester-container")
 require("prototypes.entities.medium-storage-container")
--- Big
 require("prototypes.entities.big-container")
 require("prototypes.entities.big-active-provider-container")
 require("prototypes.entities.big-buffer-container")
 require("prototypes.entities.big-passive-provider-container")
 require("prototypes.entities.big-requester-container")
 require("prototypes.entities.big-storage-container")
--- -- Fluid storages
 require("prototypes.entities.fluid-storage-1")
 require("prototypes.entities.fluid-storage-2")
--- -- Belts
--- Advanced
 require("prototypes.entities.advanced-belt-animation-set") -- Must be loaded before the belts set
 require("prototypes.entities.advanced-splitter")
 require("prototypes.entities.advanced-transport-belt")
 require("prototypes.entities.advanced-underground-belt")
--- Superior
 require("prototypes.entities.superior-belt-animation-set") -- Must be loaded before the belts set
 require("prototypes.entities.superior-splitter")
 require("prototypes.entities.superior-transport-belt")
 require("prototypes.entities.superior-underground-belt")
--- Loaders
 require("prototypes.entities.loaders") -- Must be required after belts
----------------------------------------------------------------------------------------
--- -- -- REMNANTS
----------------------------------------------------------------------------------------
 require("prototypes.entities.remnants.advanced-tank-remnant")
 require("prototypes.entities.remnants.fusion-reactor-remnant")
 require("prototypes.entities.remnants.antimatter-reactor-remnant")
@@ -78,23 +60,13 @@ require("prototypes.entities.remnants.kr-mineral-water-pumpjack-remnant")
 require("prototypes.entities.remnants.matter-plant-remnant")
 require("prototypes.entities.remnants.superior-inserter-remnant")
 require("prototypes.entities.remnants.turret-remnant")
--- -- Belts
--- Advanced
 require("prototypes.entities.remnants.advanced-splitter-remnant")
 require("prototypes.entities.remnants.advanced-transport-belt-remnant")
 require("prototypes.entities.remnants.advanced-underground-belt-remnant")
--- Superior
 require("prototypes.entities.remnants.superior-splitter-remnant")
 require("prototypes.entities.remnants.superior-transport-belt-remnant")
 require("prototypes.entities.remnants.superior-underground-belt-remnant")
----------------------------------------------------------------------------------------
--- -- -- VEHICLES
----------------------------------------------------------------------------------------
 require("prototypes.entities.advanced-tank")
 require("prototypes.entities.nuclear-locomotive")
----------------------------------------------------------------------------------------
--- -- -- Explosions&projectiles
----------------------------------------------------------------------------------------
 require("prototypes.entities.explosions")
 require("prototypes.entities.projectiles")
----------------------------------------------------------------------------------------
