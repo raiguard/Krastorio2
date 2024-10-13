c9eacbf0ec0609602ff4e0034a70db9817ca00c1
Caleb Heuer <ch@raiguard.me>
2024-10-12 22:56:39 -0600

    Update for 2.0.1 meld

--- prototypes/vehicles/nuclear-locomotive.lua
+++ prototypes/vehicles/nuclear-locomotive.lua
@@ -1,44 +1,5 @@
 local nuclear_smoke_color = { r = 0.5, g = 0.55, b = 0.45, a = 0.25 }
-local hit_effects = require("__base__.prototypes.entity.hit-effects")
-local sounds = require("__base__.prototypes.entity.sounds")
 local meld = require("__core__.lualib.meld")
-
--- TODO: Inline this in data:extend after 2.0.1 is released.
-local loco = table.deepcopy(data.raw.locomotive.locomotive)
-meld(loco, {
-  name = "kr-nuclear-locomotive",
-  icon = "__Krastorio2Assets__/icons/vehicles/nuclear-locomotive.png",
-  minable = { result = "kr-nuclear-locomotive" },
-  max_health = 1500,
-  equipment_grid = "kr-locomotive-grid",
-  weight = 12000,
-  max_speed = 1.15725,
-  max_power = "3MW",
-  reversing_power_modifier = 0.75,
-  braking_force = 40,
-  color = { r = 0.92, g = 0.07, b = 0, a = 0.5 },
-  energy_source = meld.overwrite({
-    type = "burner",
-    fuel_categories = { "nuclear" },
-    effectivity = 0.1,
-    fuel_inventory_size = 1,
-    burnt_inventory_size = 1,
-    smoke = {
-      {
-        name = "nuclear-train-smoke",
-        deviation = { 0.3, 0.3 },
-        frequency = 60,
-        position = { 0, 0 },
-        starting_frame = 0,
-        starting_frame_deviation = 60,
-        height = 2,
-        height_deviation = 0.5,
-        starting_vertical_speed = 0.2,
-        starting_vertical_speed_deviation = 0.1,
-      },
-    },
-  }),
-})
 
 data:extend({
   {
@@ -65,7 +26,40 @@ data:extend({
     place_result = "kr-nuclear-locomotive",
     stack_size = 5,
   },
-  loco,
+  meld(table.deepcopy(data.raw.locomotive.locomotive), {
+    name = "kr-nuclear-locomotive",
+    icon = "__Krastorio2Assets__/icons/vehicles/nuclear-locomotive.png",
+    minable = { result = "kr-nuclear-locomotive" },
+    max_health = 1500,
+    equipment_grid = "kr-locomotive-grid",
+    weight = 12000,
+    max_speed = 1.15725,
+    max_power = "3MW",
+    reversing_power_modifier = 0.75,
+    braking_force = 40,
+    color = { r = 0.92, g = 0.07, b = 0, a = 0.5 },
+    energy_source = meld.overwrite({
+      type = "burner",
+      fuel_categories = { "nuclear" },
+      effectivity = 0.1,
+      fuel_inventory_size = 1,
+      burnt_inventory_size = 1,
+      smoke = {
+        {
+          name = "nuclear-train-smoke",
+          deviation = { 0.3, 0.3 },
+          frequency = 60,
+          position = { 0, 0 },
+          starting_frame = 0,
+          starting_frame_deviation = 60,
+          height = 2,
+          height_deviation = 0.5,
+          starting_vertical_speed = 0.2,
+          starting_vertical_speed_deviation = 0.1,
+        },
+      },
+    }),
+  }),
   {
     type = "trivial-smoke",
     name = "nuclear-train-smoke",
