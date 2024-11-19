cde8b86b53c61798604fde1a0462a25751ccf203
Caleb Heuer <ch@raiguard.me>
2024-11-19 20:48:38 +0900

    Simplify squeak through 2 code

--- prototypes/compatibility/squeak-through-2.lua
+++ prototypes/compatibility/squeak-through-2.lua
@@ -2,93 +2,86 @@ if not mods["squeak-through-2"] then
   return
 end
 
---- @param type string
---- @param name string
-local function add_exclusion(type, name)
-  local prototype = data.raw[type][name]
-  if prototype then
-    prototype.squeak_behaviour = false --- @diagnostic disable-line
-  end
-end
+--- @diagnostic disable:inject-field
 
-add_exclusion("accumulator", "kr-energy-storage")
-add_exclusion("accumulator", "kr-intergalactic-transceiver")
+data.raw["accumulator"]["kr-energy-storage"].squeak_behavior = false
+data.raw["accumulator"]["kr-intergalactic-transceiver"].squeak_behavior = false
 
-add_exclusion("ammo-turret", "kr-railgun-turret")
-add_exclusion("ammo-turret", "kr-rocket-turret")
+data.raw["ammo-turret"]["kr-railgun-turret"].squeak_behavior = false
+data.raw["ammo-turret"]["kr-rocket-turret"].squeak_behavior = false
 
-add_exclusion("assembling-machine", "kr-advanced-chemical-plant")
-add_exclusion("assembling-machine", "kr-advanced-furnace")
-add_exclusion("assembling-machine", "kr-air-filter")
-add_exclusion("assembling-machine", "kr-atmospheric-condenser")
-add_exclusion("assembling-machine", "kr-bio-lab")
-add_exclusion("assembling-machine", "kr-electrolysis-plant")
-add_exclusion("assembling-machine", "kr-filtration-plant")
-add_exclusion("assembling-machine", "kr-fuel-refinery")
-add_exclusion("assembling-machine", "kr-fuel-refinery")
-add_exclusion("assembling-machine", "kr-greenhouse")
-add_exclusion("assembling-machine", "kr-matter-associator")
-add_exclusion("assembling-machine", "kr-matter-plant")
-add_exclusion("assembling-machine", "kr-quantum-computer")
+data.raw["assembling-machine"]["kr-advanced-chemical-plant"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-advanced-furnace"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-air-filter"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-atmospheric-condenser"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-bio-lab"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-electrolysis-plant"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-filtration-plant"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-fuel-refinery"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-fuel-refinery"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-greenhouse"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-matter-associator"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-matter-plant"].squeak_behavior = false
+data.raw["assembling-machine"]["kr-quantum-computer"].squeak_behavior = false
 
-add_exclusion("beacon", "kr-singularity-beacon")
+data.raw["beacon"]["kr-singularity-beacon"].squeak_behavior = false
 
-add_exclusion("burner-generator", "kr-antimatter-reactor")
+data.raw["burner-generator"]["kr-antimatter-reactor"].squeak_behavior = false
 
-add_exclusion("container", "kr-shelter-container")
-add_exclusion("container", "kr-shelter-plus-container")
-add_exclusion("container", "kr-storehouse")
-add_exclusion("container", "kr-warehouse")
+data.raw["container"]["kr-shelter-container"].squeak_behavior = false
+data.raw["container"]["kr-shelter-plus-container"].squeak_behavior = false
+data.raw["container"]["kr-storehouse"].squeak_behavior = false
+data.raw["container"]["kr-warehouse"].squeak_behavior = false
 
-add_exclusion("electric-energy-interface", "kr-activated-intergalactic-transceiver")
-add_exclusion("electric-energy-interface", "kr-shelter")
-add_exclusion("electric-energy-interface", "kr-shelter-plus")
-add_exclusion("electric-energy-interface", "kr-wind-turbine")
+data.raw["electric-energy-interface"]["kr-activated-intergalactic-transceiver"].squeak_behavior = false
+data.raw["electric-energy-interface"]["kr-shelter"].squeak_behavior = false
+data.raw["electric-energy-interface"]["kr-shelter-plus"].squeak_behavior = false
+data.raw["electric-energy-interface"]["kr-wind-turbine"].squeak_behavior = false
 
-add_exclusion("furnace", "kr-air-purifier")
-add_exclusion("furnace", "kr-crusher")
-add_exclusion("furnace", "kr-crusher")
-add_exclusion("furnace", "kr-flare-stack")
-add_exclusion("furnace", "kr-fusion-reactor")
-add_exclusion("furnace", "kr-stabilizer-charging-station")
+data.raw["furnace"]["kr-air-purifier"].squeak_behavior = false
+data.raw["furnace"]["kr-crusher"].squeak_behavior = false
+data.raw["furnace"]["kr-crusher"].squeak_behavior = false
+data.raw["furnace"]["kr-flare-stack"].squeak_behavior = false
+data.raw["furnace"]["kr-fusion-reactor"].squeak_behavior = false
+data.raw["furnace"]["kr-stabilizer-charging-station"].squeak_behavior = false
 
-add_exclusion("generator", "kr-advanced-steam-turbine")
-add_exclusion("generator", "kr-gas-power-station")
+data.raw["generator"]["kr-advanced-steam-turbine"].squeak_behavior = false
+data.raw["generator"]["kr-gas-power-station"].squeak_behavior = false
 
-add_exclusion("lab", "biusart_lab")
-add_exclusion("lab", "kr-singularity-lab")
+data.raw["lab"]["biusart_lab"].squeak_behavior = false
+data.raw["lab"]["kr-singularity-lab"].squeak_behavior = false
 
-add_exclusion("loader-1x1", "kr-express-loader")
-add_exclusion("loader-1x1", "kr-fast-loader")
-add_exclusion("loader-1x1", "kr-loader")
-add_exclusion("loader-1x1", "kr-se-loader")
-add_exclusion("loader-1x1", "kr-superior-loader")
+data.raw["loader-1x1"]["kr-express-loader"].squeak_behavior = false
+data.raw["loader-1x1"]["kr-fast-loader"].squeak_behavior = false
+data.raw["loader-1x1"]["kr-loader"].squeak_behavior = false
+data.raw["loader-1x1"]["kr-se-loader"].squeak_behavior = false
+data.raw["loader-1x1"]["kr-superior-loader"].squeak_behavior = false
 
-add_exclusion("logistic-container", "kr-active-provider-storehouse")
-add_exclusion("logistic-container", "kr-active-provider-warehouse")
-add_exclusion("logistic-container", "kr-buffer-storehouse")
-add_exclusion("logistic-container", "kr-buffer-warehouse")
-add_exclusion("logistic-container", "kr-passive-provider-storehouse")
-add_exclusion("logistic-container", "kr-passive-provider-warehouse")
-add_exclusion("logistic-container", "kr-requester-storehouse")
-add_exclusion("logistic-container", "kr-requester-warehouse")
-add_exclusion("logistic-container", "kr-storage-storehouse")
-add_exclusion("logistic-container", "kr-storage-warehouse")
+data.raw["logistic-container"]["kr-active-provider-storehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-active-provider-warehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-buffer-storehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-buffer-warehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-passive-provider-storehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-passive-provider-warehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-requester-storehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-requester-warehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-storage-storehouse"].squeak_behavior = false
+data.raw["logistic-container"]["kr-storage-warehouse"].squeak_behavior = false
 
-add_exclusion("mining-drill", "kr-mineral-water-pumpjack")
-add_exclusion("mining-drill", "kr-quarry-drill")
+data.raw["mining-drill"]["kr-mineral-water-pumpjack"].squeak_behavior = false
+data.raw["mining-drill"]["kr-quarry-drill"].squeak_behavior = false
 
-add_exclusion("radar", "kr-sentinel")
+data.raw["radar"]["kr-sentinel"].squeak_behavior = false
 
-add_exclusion("roboport", "kr-big-roboport")
-add_exclusion("roboport", "kr-small-roboport")
+data.raw["roboport"]["kr-big-roboport"].squeak_behavior = false
+data.raw["roboport"]["kr-small-roboport"].squeak_behavior = false
 
-add_exclusion("simple-entity", "kr-planetary-teleporter-collision-1")
-add_exclusion("simple-entity", "kr-planetary-teleporter-collision-2")
-add_exclusion("simple-entity", "kr-planetary-teleporter-collision-3")
-add_exclusion("simple-entity", "kr-planetary-teleporter-front-layer")
+data.raw["simple-entity"]["kr-planetary-teleporter-collision-1"].squeak_behavior = false
+data.raw["simple-entity"]["kr-planetary-teleporter-collision-2"].squeak_behavior = false
+data.raw["simple-entity"]["kr-planetary-teleporter-collision-3"].squeak_behavior = false
+data.raw["simple-entity"]["kr-planetary-teleporter-front-layer"].squeak_behavior = false
 
-add_exclusion("solar-panel", "kr-advanced-solar-panel")
+data.raw["solar-panel"]["kr-advanced-solar-panel"].squeak_behavior = false
 
-add_exclusion("storage-tank", "big-storage-tank")
-add_exclusion("storage-tank", "huge-storage-tank")
+data.raw["storage-tank"]["big-storage-tank"].squeak_behavior = false
+data.raw["storage-tank"]["huge-storage-tank"].squeak_behavior = false
