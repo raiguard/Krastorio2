8cc003c3ec7762fe4ae3044dae2982422294dd5a
Caleb Heuer <ch@raiguard.me>
2024-11-23 20:53:19 +0900

    Convert robot changes

--- prototypes/updates/base/entities.lua
+++ prototypes/updates/base/entities.lua
@@ -1,4 +1,5 @@
 local data_util = require("data-util")
+local sounds = require("__base__.prototypes.entity.sounds")
 
 --- Change the vehicle to the given grid and update the equipment grid with the categories of the vehicle's former grid.
 --- @param vehicle_type string
@@ -72,6 +73,93 @@ data.raw.car["tank"].localised_name = { "entity-name.kr-armored-vehicle" }
 
 convert_equipment_grid("cargo-wagon", "cargo-wagon", "kr-wagons-grid")
 data.raw["cargo-wagon"]["cargo-wagon"].allow_robot_dispatch_in_automatic_mode = true
+
+data.raw["combat-robot"]["defender"].time_to_live = 60 * 120
+data.raw["combat-robot"]["defender"].attack_parameters = {
+  type = "projectile",
+  cooldown = 20,
+  projectile_center = { 0, 1 },
+  projectile_creation_distance = 0.6,
+  range = 20,
+  sound = sounds.light_gunshot,
+  ammo_category = "bullet",
+  ammo_type = {
+    action = {
+      type = "direct",
+      action_delivery = {
+        type = "instant",
+        source_effects = {
+          type = "create-explosion",
+          entity_name = "explosion-gunshot-small",
+        },
+        target_effects = {
+          {
+            type = "create-entity",
+            entity_name = "explosion-hit",
+          },
+          {
+            type = "damage",
+            damage = { amount = 8, type = "physical" },
+          },
+        },
+      },
+    },
+  },
+}
+
+data.raw["combat-robot"]["destroyer"].time_to_live = 60 * 180
+data.raw["combat-robot"]["destroyer"].attack_parameters = {
+  type = "beam",
+  ammo_category = "laser",
+  cooldown = 20,
+  damage_modifier = 2,
+  range = 25,
+  ammo_type = {
+    category = "laser",
+    action = {
+      type = "direct",
+      action_delivery = {
+        type = "beam",
+        beam = "electric-beam",
+        max_length = 25,
+        duration = 20,
+        source_offset = { 0.15, -0.5 },
+      },
+    },
+  },
+}
+
+data.raw["combat-robot"]["distractor"].time_to_live = 60 * 120
+data.raw["combat-robot"]["distractor"].attack_parameters = {
+  type = "beam",
+  ammo_category = "laser",
+  cooldown = 20,
+  damage_modifier = 2,
+  projectile_center = { 0, 0 },
+  projectile_creation_distance = 0.6,
+  range = 20,
+  sound = make_laser_sounds(),
+  ammo_type = {
+    category = "laser",
+    action = {
+      type = "direct",
+      action_delivery = {
+        type = "beam",
+        beam = "laser-beam",
+        max_length = 20,
+        duration = 20,
+        --starting_speed = 0.3
+      },
+    },
+  },
+}
+
+if not mods["boblogistics"] then
+  data.raw["construction-robot"]["construction-robot"].speed = 0.09257
+  data.raw["construction-robot"]["construction-robot"].max_energy = "3MJ"
+  data.raw["construction-robot"]["construction-robot"].max_health = 50
+  data.raw["construction-robot"]["construction-robot"].max_payload_size = 2
+end
 
 data.raw.container["crash-site-chest-1"].minable = small_crash_site_minable
 
@@ -165,6 +253,13 @@ data_util.remove_lab_input("lab", kr_optimization_tech_card_name)
 
 data.raw.lamp["small-lamp"].glow_size = 8 --6
 
+if not mods["boblogistics"] then
+  data.raw["logistic-robot"]["logistic-robot"].speed = 0.0694
+  data.raw["logistic-robot"]["logistic-robot"].max_energy = "3MJ"
+  data.raw["logistic-robot"]["logistic-robot"].max_health = 50
+  data.raw["logistic-robot"]["logistic-robot"].max_payload_size = 7
+end
+
 convert_equipment_grid("locomotive", "locomotive", "kr-locomotive-grid")
 
 data.raw["mining-drill"]["burner-mining-drill"].energy_source.emissions_per_minute = { pollution = 5 }
@@ -242,6 +337,11 @@ end
 data.raw.radar["radar"].fast_replaceable_group = "radar"
 
 data.raw["rocket-silo"]["rocket-silo"].localised_description = { "entity-description.kr-rocket-silo" }
+
+data.raw["roboport"]["roboport"].charging_energy = "1000kW"
+data.raw["roboport"]["roboport"].energy_source.input_flow_limit = "5MW"
+data.raw["roboport"]["roboport"].material_slots_count = 3
+data.raw["roboport"]["roboport"].robot_slots_count = 3
 
 data.raw["simple-entity-with-owner"]["crash-site-spaceship-wreck-small-1"].minable = small_crash_site_minable
 
--- prototypes/vanilla-changes/mandatory-vanilla-updates.lua
+++ prototypes/vanilla-changes/mandatory-vanilla-updates.lua
@@ -1,2 +1,1 @@
 require("mandatory.damage-technologies-bonuses") -- Rebalance the damage donus from technologies
-require("mandatory.vanilla-robots-changes") -- Rebalance vanilla drones
