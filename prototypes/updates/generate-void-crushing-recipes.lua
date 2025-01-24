2e3e73188bd432c80e4e7d22e0a49daddbf38f38
Caleb Heuer <ch@raiguard.me>
2025-01-24 16:13:29 -0700

    Add locale for void crushing recipes
    Now helpers.check_prototype_translations() passes!

--- prototypes/updates/generate-void-crushing-recipes.lua
+++ prototypes/updates/generate-void-crushing-recipes.lua
@@ -1,3 +1,6 @@
+local flib_locale = require("__flib__.locale")
+local flib_prototypes = require("__flib__.prototypes")
+
 --- @type table<string, boolean>
 local crushing_ingredients = {}
 for _, recipe in pairs(data.raw.recipe) do
@@ -8,23 +11,23 @@ for _, recipe in pairs(data.raw.recipe) do
   end
 end
 
-for prototype_type in pairs(defines.prototypes.item) do
-  if prototype_type ~= "module" then
-    for item_name in pairs(data.raw[prototype_type] or {}) do
-      if not crushing_ingredients[item_name] then
-        data:extend({
-          {
-            type = "recipe",
-            name = "kr-crush-" .. item_name,
-            icon = "__Krastorio2Assets__/icons/recipes/trash.png",
-            category = "void-crushing",
-            hidden = true,
-            hide_from_stats = true,
-            energy_required = 2,
-            ingredients = { { type = "item", name = item_name, amount = 1 } },
-          },
-        })
-      end
-    end
+for _, item in pairs(flib_prototypes.all("item")) do
+  if item.type ~= "module" and not crushing_ingredients[item.name] then
+    data:extend({
+      {
+        type = "recipe",
+        name = "kr-crush-" .. item.name,
+        localised_name = {
+          "recipe-name.kr-crush",
+          flib_locale.of(item),
+        },
+        icon = "__Krastorio2Assets__/icons/recipes/trash.png",
+        category = "void-crushing",
+        hidden = true,
+        hide_from_stats = true,
+        energy_required = 2,
+        ingredients = { { type = "item", name = item.name, amount = 1 } },
+      },
+    })
   end
 end
