160ab9a982e560e615519d91cefc30ccf2a36a3a
Caleb Heuer <ch@raiguard.me>
2025-02-11 16:26:52 -0700

    Group roboport variants together in factoriopedia

--- prototypes/updates/generate-roboport-variations.lua
+++ prototypes/updates/generate-roboport-variations.lua
@@ -1,16 +1,17 @@
 local roboports = {}
 
-for _, roboport in pairs(data.raw.roboport) do
+for roboport_name, roboport in pairs(data.raw.roboport) do
   -- Fast replaceable group (for ease of variation switching)
-  local fr_group = roboport.fast_replaceable_group or roboport.name
+  local fr_group = roboport.fast_replaceable_group or roboport_name
   roboport.fast_replaceable_group = fr_group
 
   -- Logistic
   local logistic_roboport = table.deepcopy(roboport)
-  logistic_roboport.name = logistic_roboport.name .. "-logistic-mode"
-  logistic_roboport.localised_name = { "entity-name.kr-logistic-roboport", { "entity-name." .. roboport.name } }
-  logistic_roboport.localised_description = { "entity-description." .. roboport.name }
-  logistic_roboport.placeable_by = { item = roboport.name, count = 1 }
+  logistic_roboport.name = roboport_name .. "-logistic-mode"
+  logistic_roboport.localised_name = { "entity-name.kr-logistic-roboport", { "entity-name." .. roboport_name } }
+  logistic_roboport.localised_description = { "entity-description." .. roboport_name }
+  logistic_roboport.factoriopedia_alternative = roboport_name
+  logistic_roboport.placeable_by = { item = roboport_name, count = 1 }
   logistic_roboport.logistics_radius =
     math.ceil(logistic_roboport.logistics_radius + logistic_roboport.logistics_radius * 0.275) -- Standard roboport has 64x64 radius
   logistic_roboport.construction_radius = 0
@@ -18,13 +19,14 @@ for _, roboport in pairs(data.raw.roboport) do
 
   -- Construction
   local construction_roboport = table.deepcopy(roboport)
-  construction_roboport.name = construction_roboport.name .. "-construction-mode"
+  construction_roboport.name = roboport_name .. "-construction-mode"
   construction_roboport.localised_name = {
     "entity-name.kr-construction-roboport",
-    { "entity-name." .. roboport.name },
+    { "entity-name." .. roboport_name },
   }
-  construction_roboport.localised_description = { "entity-description." .. roboport.name }
-  construction_roboport.placeable_by = { item = roboport.name, count = 1 }
+  construction_roboport.localised_description = { "entity-description." .. roboport_name }
+  construction_roboport.factoriopedia_alternative = roboport_name
+  construction_roboport.placeable_by = { item = roboport_name, count = 1 }
   construction_roboport.construction_radius =
     math.ceil(construction_roboport.construction_radius + construction_roboport.construction_radius * 0.25)
   construction_roboport.logistics_connection_distance =
