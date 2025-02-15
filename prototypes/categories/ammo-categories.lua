7b0344c3853dd6b85194ff4c51a55237ca04c63b
Caleb Heuer <ch@raiguard.me>
2025-02-15 00:16:09 -0700

    Relocate ammo categories

--- prototypes/categories/ammo-categories.lua
+++ prototypes/categories/ammo-categories.lua
@@ -3,21 +3,25 @@ data:extend({
     type = "ammo-category",
     name = "impulse-rifle",
     bonus_gui_order = "k",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
     name = "heavy-rocket",
     bonus_gui_order = "i",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
     name = "railgun-shell",
     bonus_gui_order = "i",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
     name = "missiles-for-turrets",
     bonus_gui_order = "q",
+    subgroup = "ammo-category",
   },
 })
 
@@ -30,15 +34,18 @@ data:extend({
     type = "ammo-category",
     name = "pistol-ammo",
     bonus_gui_order = "r",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
     name = "rifle-ammo",
     bonus_gui_order = "s",
+    subgroup = "ammo-category",
   },
   {
     type = "ammo-category",
     name = "anti-material-rifle-ammo",
     bonus_gui_order = "t",
+    subgroup = "ammo-category",
   },
 })
