3b8441dc3fea50990d5167eb8ab668390f5109f6
Caleb Heuer <ch@raiguard.me>
2025-03-22 12:59:01 -0600

    Fix pipette not working on roboport variations

--- prototypes/updates/generate-roboport-variations.lua
+++ prototypes/updates/generate-roboport-variations.lua
@@ -1,9 +1,16 @@
+local flib_prototypes = require("__flib__.prototypes")
+
 --- @type data.RoboportPrototype[]
 local roboports = {}
 
 for roboport_name, roboport in pairs(data.raw.roboport) do
-  local fr_group = roboport.fast_replaceable_group or roboport_name
-  roboport.fast_replaceable_group = fr_group
+  local fast_replaceable_group = roboport.fast_replaceable_group or roboport_name
+  roboport.fast_replaceable_group = fast_replaceable_group
+
+  local placeable_by
+  if flib_prototypes.find("item", roboport_name) then
+    placeable_by = { item = roboport_name, count = 1 }
+  end
 
   -- Logistic
   local logistic_roboport = table.deepcopy(roboport)
@@ -11,11 +18,14 @@ for roboport_name, roboport in pairs(data.raw.roboport) do
   logistic_roboport.localised_name = { "entity-name.kr-logistic-roboport", { "entity-name." .. roboport_name } }
   logistic_roboport.localised_description = { "entity-description." .. roboport_name }
   logistic_roboport.factoriopedia_alternative = roboport_name
+  if flib_prototypes.find("item", roboport_name) then
+    logistic_roboport.placeable_by = placeable_by
+  end
   logistic_roboport.logistics_radius =
     math.ceil(logistic_roboport.logistics_radius + logistic_roboport.logistics_radius * 0.275) -- Standard roboport has 64x64 radius
   logistic_roboport.logistics_connection_distance = logistic_roboport.logistics_radius
   logistic_roboport.construction_radius = 0
-  logistic_roboport.fast_replaceable_group = fr_group
+  logistic_roboport.fast_replaceable_group = fast_replaceable_group
 
   -- Construction
   local construction_roboport = table.deepcopy(roboport)
@@ -26,12 +36,15 @@ for roboport_name, roboport in pairs(data.raw.roboport) do
   }
   construction_roboport.localised_description = { "entity-description." .. roboport_name }
   construction_roboport.factoriopedia_alternative = roboport_name
+  if flib_prototypes.find("item", roboport_name) then
+    construction_roboport.placeable_by = placeable_by
+  end
   construction_roboport.construction_radius =
     math.ceil(construction_roboport.construction_radius + construction_roboport.construction_radius * 0.25)
   construction_roboport.logistics_connection_distance =
     math.ceil(math.min(construction_roboport.logistics_radius * 1.5, construction_roboport.construction_radius))
   construction_roboport.logistics_radius = 0
-  construction_roboport.fast_replaceable_group = fr_group
+  construction_roboport.fast_replaceable_group = fast_replaceable_group
 
   -- Directly extending data her would cause an infinite loop, so we add to an array before extending the array all at once.
   roboports[#roboports + 1] = logistic_roboport
